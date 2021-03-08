da1a98dfc9e4eaa298fd1adc45e269f0db391cde
Shlomp <shlomomatichin@gmail.com>
2021-03-08 15:06:33 +0200

    tested fetching data configuration variable from project scope

--- tests/003_configuration_variable/main.tf
+++ tests/003_configuration_variable/main.tf
@@ -1,10 +1,24 @@
 data "env0_configuration_variable" "region" {
   name = "AWS_DEFAULT_REGION"
 }
-
 output "region_value" {
   value = data.env0_configuration_variable.region.value
 }
 output "region_id" {
   value = data.env0_configuration_variable.region.id
+}
+
+
+data "env0_project" "default" {
+  name = "Default Organization Project"
+}
+data "env0_configuration_variable" "region_in_project" {
+  name       = "AWS_DEFAULT_REGION"
+  project_id = data.env0_project.default.id
+}
+output "region_in_project_value" {
+  value = data.env0_configuration_variable.region_in_project.value
+}
+output "region_in_project_id" {
+  value = data.env0_configuration_variable.region_in_project.id
 }
