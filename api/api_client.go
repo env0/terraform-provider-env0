b54e604f07f49b8a49aa2c5b485e3c0283fdc90b
Shlomp <shlomomatichin@gmail.com>
2021-03-22 19:16:11 +0200

    cr: removed logic.go

--- api/api_client.go
+++ api/api_client.go
@@ -38,3 +38,15 @@ func (self *ApiClient) normalizeEndpoint() {
 		self.Endpoint = self.Endpoint[:len(self.Endpoint)-1]
 	}
 }
+
+func (self *ApiClient) organizationId() (string, error) {
+	if self.cachedOrganizationId != "" {
+		return self.cachedOrganizationId, nil
+	}
+	organization, err := self.Organization()
+	if err != nil {
+		return "", nil
+	}
+	self.cachedOrganizationId = organization.Id
+	return self.cachedOrganizationId, nil
+}
