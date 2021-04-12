4f44b7694f6aa3a456da37b094cd7a64db5e2867
Yaron Yarimi <yaron.yarimi@env0.com>
2021-04-12 15:54:49 +0300

    use rest client

--- internal/api/api_suite_test.go
+++ internal/api/api_suite_test.go
@@ -7,15 +7,14 @@ import (
 	. "github.com/onsi/gomega"
 
 	"github.com/env0/terraform-provider-env0/internal/api"
+	"github.com/env0/terraform-provider-env0/internal/rest"
 )
 
 var apiClient *api.ApiClient
 
 var _ = BeforeSuite(func() {
-	var err error
-	apiClient, err = api.NewClientFromEnv()
-	Expect(err).To(BeNil())
-	Expect(apiClient).ToNot(BeNil())
+	restClient, _ := rest.NewRestClientFromEnv()
+	apiClient = api.NewApiClient(restClient)
 })
 
 func TestApi(t *testing.T) {
