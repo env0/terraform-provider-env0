6573bdbc58ee07b608d73a48a5486f3263635f73
Yaron Yarimi <yaron.yarimi@env0.com>
2021-04-13 15:55:34 +0300

    add gomock controller

--- internal/api/api_suite_test.go
+++ internal/api/api_suite_test.go
@@ -1,20 +1,35 @@
 package api_test
 
 import (
+	"github.com/golang/mock/gomock"
 	"testing"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
 	"github.com/env0/terraform-provider-env0/internal/api"
-	"github.com/env0/terraform-provider-env0/internal/rest"
+	"github.com/env0/terraform-provider-env0/internal/http/mocks"
 )
 
-var apiClient *api.ApiClient
+const organizationId = "organization0"
+
+var (
+	ctrl           *gomock.Controller
+	mockHttpClient *mocks.MockHttpClientInterface
+	apiClient      *api.ApiClient
+)
 
 var _ = BeforeSuite(func() {
-	restClient, _ := rest.NewRestClientFromEnv()
-	apiClient = api.NewApiClient(restClient)
+	ctrl = gomock.NewController(GinkgoT())
+})
+
+var _ = BeforeEach(func() {
+	mockHttpClient = mocks.NewMockHttpClientInterface(ctrl)
+	apiClient = api.NewApiClient(mockHttpClient, organizationId)
+})
+
+var _ = AfterEach(func() {
+	ctrl.Finish()
 })
 
 func TestApi(t *testing.T) {
