2e87dacf977da673568918d1e159c470c0df4c1b
Yaron Yarimi <yaron.yarimi@env0.com>
2021-04-13 15:53:56 +0300

    changed org id member and func names

--- internal/api/api_client.go
+++ internal/api/api_client.go
@@ -5,24 +5,25 @@ import (
 )
 
 type ApiClient struct {
-	client               HttpClientInterface
-	cachedOrganizationId string
+	client         HttpClientInterface
+	organizationId string
 }
 
-func NewApiClient(client HttpClientInterface) *ApiClient {
+func NewApiClient(client HttpClientInterface, organizationId string) *ApiClient {
 	return &ApiClient{
-		client: client,
+		client:         client,
+		organizationId: organizationId,
 	}
 }
 
-func (self *ApiClient) organizationId() (string, error) {
-	if self.cachedOrganizationId != "" {
-		return self.cachedOrganizationId, nil
+func (self *ApiClient) getOrganizationId() (string, error) {
+	if self.organizationId != "" {
+		return self.organizationId, nil
 	}
 	organization, err := self.Organization()
 	if err != nil {
 		return "", nil
 	}
-	self.cachedOrganizationId = organization.Id
-	return self.cachedOrganizationId, nil
+	self.organizationId = organization.Id
+	return self.organizationId, nil
 }
