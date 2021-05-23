2d79094c079f0b9fcf4c96ceede7cac8b80083a0
Yaron Yarimi <yaron.yarimi@env0.com>
2021-05-23 13:42:36 +0300

    fix folder names

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
 go fmt
-(cd env0tfprovider && go fmt)
-(cd env0apiclient && go fmt)
+(cd env0 && go fmt)
+(cd client && go fmt)
 CGO_ENABLED=0 go build -a -tags netgo -ldflags '-w -extldflags "-static"' -o terraform-provider-env0
