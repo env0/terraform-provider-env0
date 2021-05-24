63640f244c4708f3cb5f3c85de11126543fb202f
Yaron Yarimi <yaron.yarimi@env0.com>
2021-05-25 01:15:58 +0300

    add tests to all methods

--- client/project_test.go
+++ client/project_test.go
@@ -11,12 +11,12 @@ const projectName = "project_test"
 var _ = Describe("Project", func() {
 	var project Project
 	mockProject := Project{
-		Id:             "id",
-		Name:           "config-key",
+		Id:             "idX",
+		Name:           "projectX",
 		OrganizationId: organizationId,
 	}
 
-	Describe("Create", func() {
+	Describe("ProjectCreate", func() {
 		BeforeEach(func() {
 			mockOrganizationIdCall(organizationId)
 
@@ -39,8 +39,58 @@ var _ = Describe("Project", func() {
 			httpCall.Times(1)
 		})
 
-		It("Should return project variable", func() {
+		It("Should return project", func() {
 			Expect(project).To(Equal(mockProject))
+		})
+	})
+
+	Describe("ProjectDelete", func() {
+		BeforeEach(func() {
+			httpCall = mockHttpClient.EXPECT().Delete("/projects/" + mockProject.Id).Return(nil)
+			apiClient.ProjectDelete(mockProject.Id)
+		})
+
+		It("Should send DELETE request with project id", func() {
+			httpCall.Times(1)
+		})
+	})
+
+	Describe("Project", func() {
+		BeforeEach(func() {
+			httpCall = mockHttpClient.EXPECT().Get("/projects/"+mockProject.Id, nil).Return(mockProject, nil)
+			project, _ = apiClient.Project(mockProject.Id)
+		})
+
+		It("Should send GET request with project id", func() {
+			httpCall.Times(1)
+		})
+
+		It("Should return project", func() {
+			Expect(project).To(Equal(mockProject))
+		})
+	})
+
+	Describe("Projects", func() {
+		var projects []Project
+		mockProjects := []Project{mockProject}
+
+		BeforeEach(func() {
+			mockOrganizationIdCall(organizationId)
+
+			httpCall = mockHttpClient.EXPECT().Get("/projects", map[string]string{"organizationId": organizationId}).Return(mockProjects, nil)
+			projects, _ = apiClient.Projects()
+		})
+
+		It("Should send GET request with project id", func() {
+			httpCall.Times(1)
+		})
+
+		It("Should get organization id", func() {
+			organizationIdCall.Times(1)
+		})
+
+		It("Should return projects", func() {
+			Expect(projects).To(Equal(mockProjects))
 		})
 	})
 })
