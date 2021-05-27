34e48732eeb85c2ed3486cb890d80367488f20ba
Yaron Yarimi <yaron.yarimi@env0.com>
2021-05-27 17:14:25 +0300

    add test unit providers

--- env0/provider_test.go
+++ env0/provider_test.go
@@ -2,10 +2,11 @@ package env0
 
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"testing"
 )
 
 var testAccProvider *schema.Provider
+var testUnitProvider *schema.Provider
+
 var testAccProviders = map[string]func() (*schema.Provider, error){
 	"env0": func() (*schema.Provider, error) {
 		if testAccProvider == nil {
@@ -15,6 +16,15 @@ var testAccProviders = map[string]func() (*schema.Provider, error){
 	},
 }
 
-func TestMain(m *testing.M) {
-
+var testUnitProviders = map[string]func() (*schema.Provider, error){
+	"env0": func() (*schema.Provider, error) {
+		if testUnitProvider == nil {
+			provider := Provider()
+			provider.ConfigureFunc = func(d *schema.ResourceData) (interface{}, error) {
+				return nil, nil
+			}
+			testUnitProvider = Provider()
+		}
+		return testUnitProvider, nil
+	},
 }
