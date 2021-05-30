fc5c33b9078cdc7798b5f4362e4e044d4f8003be
Yaron Yarimi <yaron.yarimi@env0.com>
2021-05-30 15:55:10 +0300

    add provider testbed

--- env0/provider_test.go
+++ env0/provider_test.go
@@ -1,30 +1,50 @@
 package env0
 
 import (
+	"github.com/env0/terraform-provider-env0/client"
+	"github.com/golang/mock/gomock"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"testing"
 )
 
-var testAccProvider *schema.Provider
-var testUnitProvider *schema.Provider
+var (
+	apiClientMock *client.MockApiClientInterface
+	ctrl          *gomock.Controller
+)
 
-var testAccProviders = map[string]func() (*schema.Provider, error){
-	"env0": func() (*schema.Provider, error) {
-		if testAccProvider == nil {
-			testAccProvider = Provider()
-		}
-		return testAccProvider, nil
-	},
+func mockApiClient(t *testing.T) (*gomock.Controller, *client.MockApiClientInterface) {
+	if ctrl == nil {
+		ctrl = gomock.NewController(t)
+	}
+
+	if apiClientMock == nil {
+		apiClientMock = client.NewMockApiClientInterface(ctrl)
+	}
+
+	return ctrl, apiClientMock
 }
 
-var testUnitProviders = map[string]func() (*schema.Provider, error){
-	"env0": func() (*schema.Provider, error) {
-		if testUnitProvider == nil {
+func testUnitProviders(ctrl *gomock.Controller) map[string]func() (*schema.Provider, error) {
+	if apiClientMock == nil {
+		apiClientMock = client.NewMockApiClientInterface(ctrl)
+	}
+
+	return map[string]func() (*schema.Provider, error){
+		"env0": func() (*schema.Provider, error) {
 			provider := Provider()
 			provider.ConfigureFunc = func(d *schema.ResourceData) (interface{}, error) {
-				return nil, nil
+				return apiClientMock, nil
 			}
-			testUnitProvider = Provider()
-		}
-		return testUnitProvider, nil
-	},
+			return provider, nil
+		},
+	}
+}
+
+func runUnitTest(t *testing.T, c resource.TestCase) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	c.ProviderFactories = testUnitProviders(ctrl)
+	resource.Test(t, c)
 }
