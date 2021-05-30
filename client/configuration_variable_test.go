f395cd614052160fcb6922138d98e500d74adffe
avner.sorek <avner.sorek@env0.com>
2021-05-30 16:58:49 +0300

    adding delete and update tests

--- client/configuration_variable_test.go
+++ client/configuration_variable_test.go
@@ -9,7 +9,7 @@ import (
 
 var _ = Describe("Configuration Variable", func() {
 	mockConfigurationVariable := ConfigurationVariable{
-		Id:             "idX",
+		Id:             "config-var-id-789",
 		Name:           "configName",
 		Value:          "configValue",
 		OrganizationId: organizationId,
@@ -63,6 +63,64 @@ var _ = Describe("Configuration Variable", func() {
 
 		It("Should return created configuration variable", func() {
 			Expect(createdConfigurationVariable).To(Equal(mockConfigurationVariable))
+		})
+	})
+
+	Describe("ConfigurationVariableDelete", func() {
+		BeforeEach(func() {
+			httpCall = mockHttpClient.EXPECT().Delete("configuration/" + mockConfigurationVariable.Id)
+			apiClient.ConfigurationVariableDelete(mockConfigurationVariable.Id)
+		})
+
+		It("Should send DELETE request with project id", func() {
+			httpCall.Times(1)
+		})
+	})
+
+	Describe("ConfigurationVariableUpdate", func() {
+		var updatedConfigurationVariable ConfigurationVariable
+
+		BeforeEach(func() {
+			mockOrganizationIdCall(organizationId)
+
+			newName := "new-" + mockConfigurationVariable.Name
+			newValue := "new-" + mockConfigurationVariable.Value
+
+			expectedUpdateRequest := []map[string]interface{}{{
+				"name":           newName,
+				"value":          newValue,
+				"id":             mockConfigurationVariable.Id,
+				"isSensitive":    mockConfigurationVariable.IsSensitive,
+				"organizationId": organizationId,
+				"scopeId":        mockConfigurationVariable.ScopeId,
+				"scope":          Scope(mockConfigurationVariable.Scope),
+				"type":           ConfigurationVariableType(mockConfigurationVariable.Type),
+			}}
+
+			httpCall = mockHttpClient.EXPECT().
+				Post("/configuration", expectedUpdateRequest, gomock.Any()).
+				Do(func(path string, request interface{}, response *[]ConfigurationVariable) {
+					*response = []ConfigurationVariable{mockConfigurationVariable}
+				})
+
+			updatedConfigurationVariable, _ = apiClient.ConfigurationVariableUpdate(
+				mockConfigurationVariable.Id,
+				newName,
+				newValue,
+				mockConfigurationVariable.IsSensitive,
+				Scope(mockConfigurationVariable.Scope),
+				mockConfigurationVariable.ScopeId,
+				ConfigurationVariableType(mockConfigurationVariable.Type),
+				nil,
+			)
+		})
+
+		It("Should send POST request with expected payload", func() {
+			httpCall.Times(1)
+		})
+
+		It("Should return configuration value received from API", func() {
+			Expect(updatedConfigurationVariable).To(Equal(mockConfigurationVariable))
 		})
 	})
 })
