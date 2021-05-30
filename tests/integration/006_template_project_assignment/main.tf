f43941290bc6672b3f1d1437507295e05926d472
eyalatzmon <eyal.atzmon@env0.com>
2021-05-30 19:41:27 +0300

    006 moved

---
+++ tests/integration/006_template_project_assignment/main.tf
@@ -0,0 +1,25 @@
+/*data "env0_project" "test_project" {
+  name = "Default Organization Project"
+}*/
+
+resource "env0_template_project_assignment" "template_project_test" {
+  template_id = env0_template.test_template.id
+  project_id  = env0_project.test_project.id
+}
+
+resource "env0_template" "test_template" {
+  name        = "Test-Template12345"
+  description = "test template"
+  type        = "terraform"
+  repository  = "https://github.com/shlomimatichin/env0-template-jupyter-gpu"
+}
+
+resource "env0_project" "test_project" {
+  name        = "Test-Project"
+  description = "Test Description"
+}
+
+
+output "env0_project_id" {
+  value = env0_project.test_project.name
+}
