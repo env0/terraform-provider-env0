4f8e41a00788f47f1ee22b5197e17666b8806019
avner.sorek <avner.sorek@env0.com>
2021-05-31 15:07:01 +0300

    better test

--- client/model_test.go
+++ client/model_test.go
@@ -5,17 +5,25 @@ import (
 
 	. "github.com/env0/terraform-provider-env0/client"
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/types"
 )
 
 var _ = Describe("Models", func() {
 	Describe("TemplateCreatePayload", func() {
-		It("Should omit Github Installation Id when it's not there", func() {
-			payload := TemplateCreatePayload{}
-			jsonPayload, _ := json.Marshal(payload)
-			var parsedPayload map[string]interface{}
-			json.Unmarshal(jsonPayload, &parsedPayload)
-			Expect(parsedPayload["githubInstallationId"]).To(BeNil())
-		})
+		DescribeTable("Github Installation Id",
+			func(value int, expected types.GomegaMatcher) {
+				payload := TemplateCreatePayload{
+					GithubInstallationId: value,
+				}
+				jsonPayload, _ := json.Marshal(payload)
+				var parsedPayload map[string]interface{}
+				json.Unmarshal(jsonPayload, &parsedPayload)
+				Expect(parsedPayload["githubInstallationId"]).To(expected)
+			},
+			Entry("Has value", 123, BeEquivalentTo(123)),
+			Entry("No value", nil, BeNil()),
+		)
 	})
 })
