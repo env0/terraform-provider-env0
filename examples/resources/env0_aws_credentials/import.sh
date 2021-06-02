982ac194949b4cd5c90b24b0a70c1a572b919d0a
eranelbaz <eranelbaz97@gmail.com>
2021-06-02 16:26:28 +0300

    update to by id by name

--- examples/resources/env0_aws_credentials/import.sh
+++ examples/resources/env0_aws_credentials/import.sh
@@ -1,2 +1,2 @@
-terraform import env0_aws_credentials.example d31a6b30-5f69-4d24-937c-22322754934e
-terraform import env0_aws_credentials.example ProductionCredentials
+terraform import env0_aws_credentials.by_id d31a6b30-5f69-4d24-937c-22322754934e
+terraform import env0_aws_credentials.by_name ProductionCredentials
