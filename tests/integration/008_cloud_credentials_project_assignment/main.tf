9132441db231b9ca7eff1cbd442c3080de594852
eranelbaz <eranelbaz97@gmail.com>
2021-06-02 17:30:41 +0300

    added example

---
+++ tests/integration/008_cloud_credentials_project_assignment/main.tf
@@ -0,0 +1,15 @@
+resource "env0_cloud_credentials_project_assignment" "example" {
+  credential_id = env0_aws_credentials.credentials.id
+  project_id = env0_project.test_project.id
+}
+
+resource "env0_project" "test_project" {
+  name        = "Test-Project"
+  description = "Test Description"
+}
+
+resource "env0_aws_credentials" "credentials" {
+  name        = "example"
+  arn         = "Example role ARN"
+  external_id = "Example external id"
+}
