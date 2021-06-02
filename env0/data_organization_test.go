2bc8b2ba5404d3420c341d85ce70b816e90b4c25
Raz Ben Simon <raz.bensimon@env0.com>
2021-06-02 15:15:08 +0300

    Chore: Add organization data tests (#81)
    * add tests to data_organization.go
    * add test_helpers file

---
+++ env0/data_organization_test.go
@@ -0,0 +1,41 @@
+package env0
+
+import (
+	"github.com/env0/terraform-provider-env0/client"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"strconv"
+	"testing"
+)
+
+func TestUnitOrganizationData(t *testing.T) {
+	resourceType := "env0_organization"
+	resourceName := "test"
+	resourceFullName := dataSourceAccessor(resourceType, resourceName)
+	organization := client.Organization{
+		Id:           "id0",
+		Name:         "name0",
+		CreatedBy:    "env0",
+		Role:         "role0",
+		IsSelfHosted: false,
+	}
+
+	testCase := resource.TestCase{
+		ProviderFactories: testUnitProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: dataSourceConfigCreate(resourceType, resourceName, make(map[string]string)),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceFullName, "id", organization.Id),
+					resource.TestCheckResourceAttr(resourceFullName, "name", organization.Name),
+					resource.TestCheckResourceAttr(resourceFullName, "created_by", organization.CreatedBy),
+					resource.TestCheckResourceAttr(resourceFullName, "role", organization.Role),
+					resource.TestCheckResourceAttr(resourceFullName, "is_self_hosted", strconv.FormatBool(organization.IsSelfHosted)),
+				),
+			},
+		},
+	}
+
+	runUnitTest(t, testCase, func(mock *client.MockApiClientInterface) {
+		mock.EXPECT().Organization().AnyTimes().Return(organization, nil)
+	})
+}
