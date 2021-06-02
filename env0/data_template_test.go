a290455d6669e4115c4302295018ac5755a885ea
eyalatzmon <eyal.atzmon@env0.com>
2021-06-02 18:59:26 +0300

    Add data template UT wip

---
+++ env0/data_template_test.go
@@ -0,0 +1,89 @@
+package env0
+
+import (
+	"testing"
+
+	"github.com/env0/terraform-provider-env0/client"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestUnitTemplateData(t *testing.T) {
+	resourceType := "env0_template"
+	resourceName := "test"
+	resourceFullName := dataSourceAccessor(resourceType, resourceName)
+	retryOnDeploy := client.TemplateRetryOn{
+		Times: 2,
+	}
+	retryOnDestroy := client.TemplateRetryOn{
+		Times: 2,
+	}
+	templateRetry := client.TemplateRetry{
+		OnDeploy:  &retryOnDeploy,
+		OnDestroy: &retryOnDestroy,
+	}
+	/*
+		Author               User             `json:"author"`
+			AuthorId             string           `json:"authorId"`
+			CreatedAt            string           `json:"createdAt"`
+			Href                 string           `json:"href"`
+			Id                   string           `json:"id"`
+			Name                 string           `json:"name"`
+			Description          string           `json:"description"`
+			OrganizationId       string           `json:"organizationId"`
+			Path                 string           `json:"path"`
+			Revision             string           `json:"revision"`
+			ProjectId            string           `json:"projectId"`
+			ProjectIds           []string         `json:"projectIds"`
+			Repository           string           `json:"repository"`
+			Retry                TemplateRetry    `json:"retry"`
+			SshKeys              []TemplateSshKey `json:"sshKeys"`
+			Type                 string           `json:"type"`
+			GithubInstallationId int              `json:"githubInstallationId"`
+			UpdatedAt            string           `json:"updatedAt"`
+			TerraformVersion     string           `json:"terraformVersion"`
+	*/
+	template := client.Template{
+		Id:                   "id0",
+		AuthorId:             "author",
+		CreatedAt:            "createdAt",
+		Href:                 "href",
+		Name:                 "name0",
+		Description:          "description",
+		OrganizationId:       "organizationId",
+		Path:                 "path",
+		Revision:             "revision",
+		ProjectId:            "projectId",
+		Repository:           "repository",
+		Retry:                templateRetry,
+		Type:                 "type",
+		GithubInstallationId: 123,
+		UpdatedAt:            "updatedAt",
+		TerraformVersion:     "0.15.1",
+	}
+
+	testCase := resource.TestCase{
+		ProviderFactories: testUnitProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: dataSourceConfigCreate(resourceType, resourceName, make(map[string]string)),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceFullName, "id", template.Id),
+					resource.TestCheckResourceAttr(resourceFullName, "author_id", template.AuthorId),
+					resource.TestCheckResourceAttr(resourceFullName, "created_at", template.CreatedAt),
+					resource.TestCheckResourceAttr(resourceFullName, "href", template.Href),
+					resource.TestCheckResourceAttr(resourceFullName, "name", template.Name),
+					resource.TestCheckResourceAttr(resourceFullName, "description", template.Description),
+					resource.TestCheckResourceAttr(resourceFullName, "organization_id", template.OrganizationId),
+					resource.TestCheckResourceAttr(resourceFullName, "path", template.Path),
+					resource.TestCheckResourceAttr(resourceFullName, "revision", template.Revision),
+					resource.TestCheckResourceAttr(resourceFullName, "project_id", template.ProjectId),
+					resource.TestCheckResourceAttr(resourceFullName, "repository", template.Repository),
+				),
+			},
+		},
+	}
+
+	runUnitTest(t, testCase, func(mock *client.MockApiClientInterface) {
+		mock.EXPECT().Template(template.Id).AnyTimes().Return(template, nil)
+	})
+}
