968f8cce5a3b1f6dd96a592b610826143fef0b93
eranelbaz <eranelbaz97@gmail.com>
2021-06-02 14:47:10 +0300

    added more tests

--- client/cloud_credentials_project_assignment_test.go
+++ client/cloud_credentials_project_assignment_test.go
@@ -48,8 +48,72 @@ var _ = Describe("Credentials Project Assignment", func() {
 			})
 
 			It("should return the error from the api call", func() {
+				Expect(actualError).ShouldNot(BeNil())
 				Expect(actualError.Error()).Should(Equal(errorInfo))
 			})
 		})
+	})
+
+	Describe("RemoveCloudCredentialsFromProject", func() {
+		errorInfo := "error"
+		var actualError error
+		BeforeEach(func() {
+			httpCall = mockHttpClient.EXPECT().
+				Delete("/credentials/deployment/" + projectId + "/project/" + credentialId).
+				Return(errors.New(errorInfo)).
+				Times(1)
+			actualError = apiClient.RemoveCloudCredentialsFromProject(projectId, credentialId)
+
+		})
+
+		It("should return the error from the api call", func() {
+			Expect(actualError).ShouldNot(BeNil())
+			Expect(actualError.Error()).Should(Equal(errorInfo))
+		})
+	})
+
+	Describe("CloudCredentialProjectAssignments", func() {
+		Describe("Successful", func() {
+			var actualResult []CloudCredentialsProjectAssignment
+
+			expectedResponse := []CloudCredentialsProjectAssignment{
+				{
+					Id:           "assigment id",
+					CredentialId: "credentialId",
+					ProjectId:    projectId,
+				},
+			}
+			BeforeEach(func() {
+				httpCall = mockHttpClient.EXPECT().
+					Get("/credentials/deployment/project/"+projectId, nil, gomock.Any()).
+					Do(func(path string, request interface{}, response *[]CloudCredentialsProjectAssignment) {
+						*response = expectedResponse
+					}).Times(1)
+				actualResult, _ = apiClient.CloudCredentialProjectAssignments(projectId)
+
+			})
+
+			It("should return the GET result", func() {
+				Expect(actualResult).To(Equal(expectedResponse))
+			})
+		})
+		Describe("On Error", func() {
+			errorInfo := "error"
+			var actualError error
+			BeforeEach(func() {
+				httpCall = mockHttpClient.EXPECT().
+					Get("/credentials/deployment/project/"+projectId, nil, gomock.Any()).
+					Return(errors.New(errorInfo)).
+					Times(1)
+				_, actualError = apiClient.CloudCredentialProjectAssignments(projectId)
+
+			})
+
+			It("should return the error from the api call", func() {
+				Expect(actualError).ShouldNot(BeNil())
+				Expect(actualError.Error()).Should(Equal(errorInfo))
+			})
+		})
+
 	})
 })
