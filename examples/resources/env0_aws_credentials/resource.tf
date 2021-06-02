3c3d61d55e0aa2ac8a56330d47e7e7664047c0e8
eranelbaz <eranelbaz97@gmail.com>
2021-06-02 16:46:09 +0300

    updated the examples

--- examples/resources/env0_aws_credentials/resource.tf
+++ examples/resources/env0_aws_credentials/resource.tf
@@ -3,12 +3,3 @@ resource "env0_aws_credentials" "credentials" {
   arn         = "Example role ARN"
   external_id = "Example external id"
 }
-
-data "env0_project" "project" {
-  name = "Default Organization Project"
-}
-
-resource "env0_cloud_credentials_project_assignment" "example" {
-  credential_id = data.env0_aws_credentials.credentials.id
-  project_id = data.env0_project.project.id
-}
