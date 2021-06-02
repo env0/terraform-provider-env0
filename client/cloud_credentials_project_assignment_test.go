f7dbc90033af515b616b003f6bbd5e06865d125f
eranelbaz <eranelbaz97@gmail.com>
2021-06-02 12:06:53 +0300

    added test file

---
+++ client/cloud_credentials_project_assignment_test.go
@@ -0,0 +1,13 @@
+package client_test
+
+import (
+	. "github.com/env0/terraform-provider-env0/client"
+	"github.com/golang/mock/gomock"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Credentials Project Assignment", func() {
+
+}
