3d5100ffb5b67bd8f22a144ead94ceb0a8b68030
eranelbaz <eranelbaz97@gmail.com>
2021-06-03 14:56:29 +0300

    rename

--- env0/resource_configuration_variable_test.go
+++ env0/resource_configuration_variable_test.go
@@ -2,6 +2,7 @@ package env0
 
 import (
 	"github.com/env0/terraform-provider-env0/client"
+	"github.com/golang/mock/gomock"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"testing"
 )
@@ -9,7 +10,7 @@ import (
 func TestUnitConfigurationVariableResource(t *testing.T) {
 	resourceType := "env0_configuration_variable"
 	resourceName := "test"
-	resourceFullName := resourceAccessor(resourceType, resourceName)
+	accessor := resourceAccessor(resourceType, resourceName)
 	configVar := client.ConfigurationVariable{
 		Id:    "id0",
 		Name:  "name0",
@@ -29,9 +30,9 @@ func TestUnitConfigurationVariableResource(t *testing.T) {
 					"value": configVar.Value,
 				}),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr(resourceFullName, "id", configVar.Id),
-					resource.TestCheckResourceAttr(resourceFullName, "name", configVar.Name),
-					resource.TestCheckResourceAttr(resourceFullName, "value", configVar.Value),
+					resource.TestCheckResourceAttr(accessor, "id", configVar.Id),
+					resource.TestCheckResourceAttr(accessor, "name", configVar.Name),
+					resource.TestCheckResourceAttr(accessor, "value", configVar.Value),
 				),
 			},
 		},
@@ -45,20 +46,20 @@ func TestUnitConfigurationVariableResource(t *testing.T) {
 					"value": configVar.Value,
 				}),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr(resourceFullName, "id", configVar.Id),
-					resource.TestCheckResourceAttr(resourceFullName, "name", configVar.Name),
-					resource.TestCheckResourceAttr(resourceFullName, "value", configVar.Value),
+					resource.TestCheckResourceAttr(accessor, "id", configVar.Id),
+					resource.TestCheckResourceAttr(accessor, "name", configVar.Name),
+					resource.TestCheckResourceAttr(accessor, "value", configVar.Value),
 				),
 			},
 			{
 				Config: resourceConfigCreate(resourceType, resourceName, map[string]string{
-					"name":  configVar.Name,
+					"name":  newConfigVar.Name,
 					"value": newConfigVar.Value,
 				}),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr(resourceFullName, "id", newConfigVar.Id),
-					resource.TestCheckResourceAttr(resourceFullName, "name", newConfigVar.Name),
-					resource.TestCheckResourceAttr(resourceFullName, "value", newConfigVar.Value),
+					resource.TestCheckResourceAttr(accessor, "id", newConfigVar.Id),
+					resource.TestCheckResourceAttr(accessor, "name", newConfigVar.Name),
+					resource.TestCheckResourceAttr(accessor, "value", newConfigVar.Value),
 				),
 			},
 		},
@@ -68,14 +69,20 @@ func TestUnitConfigurationVariableResource(t *testing.T) {
 		mock.EXPECT().ConfigurationVariableCreate(configVar.Name, configVar.Value, false, client.ScopeGlobal, "", client.ConfigurationVariableTypeEnvironment,
 			nil).Times(1).Return(configVar, nil)
 		mock.EXPECT().ConfigurationVariables(client.ScopeGlobal, "").Times(1).Return([]client.ConfigurationVariable{configVar}, nil)
+		mock.EXPECT().ConfigurationVariableDelete(configVar.Id).Times(1).Return(nil)
 	})
 
 	runUnitTest(t, updateTestCase, func(mock *client.MockApiClientInterface) {
 		mock.EXPECT().ConfigurationVariableCreate(configVar.Name, configVar.Value, false, client.ScopeGlobal, "", client.ConfigurationVariableTypeEnvironment,
 			nil).Times(1).Return(configVar, nil)
-		mock.EXPECT().ConfigurationVariables(client.ScopeGlobal, "").Times(1).Return([]client.ConfigurationVariable{configVar}, nil)
+		gomock.InOrder(
+			mock.EXPECT().ConfigurationVariables(client.ScopeGlobal, "").Return([]client.ConfigurationVariable{configVar}, nil),
+			mock.EXPECT().ConfigurationVariables(client.ScopeGlobal, "").Return([]client.ConfigurationVariable{configVar}, nil),
+			mock.EXPECT().ConfigurationVariables(client.ScopeGlobal, "").Return([]client.ConfigurationVariable{newConfigVar}, nil),
+		)
 		mock.EXPECT().ConfigurationVariableUpdate(newConfigVar.Id, newConfigVar.Name, newConfigVar.Value, false, client.ScopeGlobal, "", client.ConfigurationVariableTypeEnvironment,
 			nil).Times(1).Return(configVar, nil)
-		mock.EXPECT().ConfigurationVariables(client.ScopeGlobal, "").Times(1).Return([]client.ConfigurationVariable{newConfigVar}, nil)
+		mock.EXPECT().ConfigurationVariableDelete(configVar.Id).Times(1).Return(nil)
+
 	})
 }
