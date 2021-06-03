8367ef748493ff8d1846b47512c323736e387028
eyalatzmon <eyal.atzmon@env0.com>
2021-06-03 12:48:09 +0300

    fmt

--- env0/data_template_test.go
+++ env0/data_template_test.go
@@ -13,52 +13,51 @@ func TestUnitTemplateData(t *testing.T) {
 	resourceName := "test"
 	resourceFullName := dataSourceAccessor(resourceType, resourceName)
 	retryOnDeploy := client.TemplateRetryOn{
-		Times: 2,
+		Times:      2,
 		ErrorRegex: "error retry on deploy",
 	}
 	retryOnDestroy := client.TemplateRetryOn{
-		Times: 3,
+		Times:      3,
 		ErrorRegex: "error retry on destroy",
 	}
 	templateRetry := client.TemplateRetry{
 		OnDeploy:  &retryOnDeploy,
 		OnDestroy: &retryOnDestroy,
 	}
-/*
-d.SetId(template.Id)
-	d.Set("name", template.Name)
-	d.Set("repository", template.Repository)
-	d.Set("path", template.Path)
-	d.Set("revision", template.Revision)
-	d.Set("type", template.Type)
-	d.Set("project_ids", template.ProjectIds)
-	d.Set("terraform_version", template.TerraformVersion)
-	d.Set("ssh_keys", template.SshKeys)
-	if template.Retry.OnDeploy != nil {
-		d.Set("retries_on_deploy", template.Retry.OnDeploy.Times)
-		d.Set("retry_on_deploy_only_when_matches_regex", template.Retry.OnDeploy.ErrorRegex)
-	} else {
-		d.Set("retries_on_deploy", 0)
-		d.Set("retry_on_deploy_only_when_matches_regex", "")
-	}
-	if template.Retry.OnDestroy != nil {
-		d.Set("retries_on_destroy", template.Retry.OnDestroy.Times)
-		d.Set("retry_on_destroy_only_when_matches_regex", template.Retry.OnDestroy.ErrorRegex)
-	} else {
-		d.Set("retries_on_destroy", 0)
-		d.Set("retry_on_destroy_only_when_matches_regex", "")
-*/
+	/*
+	   d.SetId(template.Id)
+	   	d.Set("name", template.Name)
+	   	d.Set("repository", template.Repository)
+	   	d.Set("path", template.Path)
+	   	d.Set("revision", template.Revision)
+	   	d.Set("type", template.Type)
+	   	d.Set("project_ids", template.ProjectIds)
+	   	d.Set("terraform_version", template.TerraformVersion)
+	   	d.Set("ssh_keys", template.SshKeys)
+	   	if template.Retry.OnDeploy != nil {
+	   		d.Set("retries_on_deploy", template.Retry.OnDeploy.Times)
+	   		d.Set("retry_on_deploy_only_when_matches_regex", template.Retry.OnDeploy.ErrorRegex)
+	   	} else {
+	   		d.Set("retries_on_deploy", 0)
+	   		d.Set("retry_on_deploy_only_when_matches_regex", "")
+	   	}
+	   	if template.Retry.OnDestroy != nil {
+	   		d.Set("retries_on_destroy", template.Retry.OnDestroy.Times)
+	   		d.Set("retry_on_destroy_only_when_matches_regex", template.Retry.OnDestroy.ErrorRegex)
+	   	} else {
+	   		d.Set("retries_on_destroy", 0)
+	   		d.Set("retry_on_destroy_only_when_matches_regex", "")
+	*/
 	template := client.Template{
-		Id:             "id0",
-		Name:           "name0",
-		Repository:     "repository",
-		Path:           "path",
-		Revision:       "revision",
-		Type:                 "terraform",
-		TerraformVersion:     "0.15.1",
+		Id:               "id0",
+		Name:             "name0",
+		Repository:       "repository",
+		Path:             "path",
+		Revision:         "revision",
+		Type:             "terraform",
+		TerraformVersion: "0.15.1",
 		//sshkeys
-		Retry:                templateRetry,
-		
+		Retry: templateRetry,
 	}
 
 	templateByName := map[string]string{
@@ -88,27 +87,27 @@ d.SetId(template.Id)
 						resource.TestCheckResourceAttr(resourceFullName, "retry_on_destroy_only_when_matches_regex", template.Retry.OnDestroy.ErrorRegex),
 						/*
 
-		d.Set("retries_on_destroy", template.Retry.OnDestroy.Times)
-		d.Set("retries_on_deploy", template.Retry.OnDeploy.Times)
-						resource.TestCheckResourceAttr(resourceFullName, "created_at", template.CreatedAt),
-						resource.TestCheckResourceAttr(resourceFullName, "href", template.Href),
-						resource.TestCheckResourceAttr(resourceFullName, "description", template.Description),
-						resource.TestCheckResourceAttr(resourceFullName, "organization_id", template.OrganizationId),
-						resource.TestCheckResourceAttr(resourceFullName, "project_id", template.ProjectId),
-						resource.TestCheckResourceAttr(resourceFullName, "terraform_version", template.TerraformVersion),
-						
-							template := client.Template{
-		Id:             "id0",
-		Name:           "name0",
-		Repository:     "repository",
-		Path:           "path",
-		Revision:       "revision",
-		Type:                 "terraform",
-		TerraformVersion:     "0.15.1",
-		//sshkeys
-		//Retry:                templateRetry,
-		
-	}*/
+							d.Set("retries_on_destroy", template.Retry.OnDestroy.Times)
+							d.Set("retries_on_deploy", template.Retry.OnDeploy.Times)
+											resource.TestCheckResourceAttr(resourceFullName, "created_at", template.CreatedAt),
+											resource.TestCheckResourceAttr(resourceFullName, "href", template.Href),
+											resource.TestCheckResourceAttr(resourceFullName, "description", template.Description),
+											resource.TestCheckResourceAttr(resourceFullName, "organization_id", template.OrganizationId),
+											resource.TestCheckResourceAttr(resourceFullName, "project_id", template.ProjectId),
+											resource.TestCheckResourceAttr(resourceFullName, "terraform_version", template.TerraformVersion),
+
+												template := client.Template{
+							Id:             "id0",
+							Name:           "name0",
+							Repository:     "repository",
+							Path:           "path",
+							Revision:       "revision",
+							Type:                 "terraform",
+							TerraformVersion:     "0.15.1",
+							//sshkeys
+							//Retry:                templateRetry,
+
+						}*/
 					),
 				},
 			},
