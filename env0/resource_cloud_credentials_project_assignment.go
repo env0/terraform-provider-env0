7ce7051c35f0cf9ab166b1e9485298dffafcff0f
Roni Frantchi <roni-frantchi@users.noreply.github.com>
2021-06-03 09:56:40 +0300

    Add Credentials-Project assignment resource (#72)
    * add creds project assignments
    
    * some provider adjustments
    
    * add client model and adapt provider
    
    * fix typo
    
    * added test file
    
    * update interface
    
    * change sign
    
    * remove fmt
    
    * remove validation flow
    
    * remove description
    
    * added error check
    
    * added more tests
    
    * update api
    
    * tpo
    
    * added to provider
    
    * added example
    
    * added docs
    
    * remove template to project
    
    * moved the docs
    
    * updated the examples
    
    * generated docs
    
    * remove ssh
    
    * fixed example
    
    * added example
    
    * fix wrong endpoint used
    
    * fmt
    
    * fix client/provider/api integration
    
    * regen mocks
    
    * fix most tests
    
    * add model and test
    
    * no 2nd run
    
    * no 2nd run
    
    Co-authored-by: eranelbaz <eranelbaz97@gmail.com>

---
+++ env0/resource_cloud_credentials_project_assignment.go
@@ -0,0 +1,85 @@
+package env0
+
+import (
+	"context"
+
+	"github.com/env0/terraform-provider-env0/client"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func resourceCloudCredentialsProjectAssignment() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceCloudCredentialsProjectAssignmentCreate,
+		ReadContext:   resourceCloudCredentialsProjectAssignmentRead,
+		DeleteContext: resourceCloudCredentialsProjectAssignmentDelete,
+
+		Schema: map[string]*schema.Schema{
+			"credential_id": {
+				Type:        schema.TypeString,
+				Description: "id of cloud credentials",
+				Required:    true,
+				ForceNew:    true,
+			},
+			"project_id": {
+				Type:        schema.TypeString,
+				Description: "id of the project",
+				Required:    true,
+				ForceNew:    true,
+			},
+		},
+	}
+}
+
+func resourceCloudCredentialsProjectAssignmentCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	apiClient := meta.(*client.ApiClient)
+
+	credentialId := d.Get("credential_id").(string)
+	projectId := d.Get("project_id").(string)
+	result, err := apiClient.AssignCloudCredentialsToProject(projectId, credentialId)
+	if err != nil {
+		return diag.Errorf("could not assign cloud credentials to project: %v", err)
+	}
+	d.SetId(getResourceId(result.CredentialId, result.ProjectId))
+	return nil
+}
+
+func resourceCloudCredentialsProjectAssignmentRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	apiClient := meta.(*client.ApiClient)
+
+	credentialId := d.Get("credential_id").(string)
+	projectId := d.Get("project_id").(string)
+	credentialsList, err := apiClient.CloudCredentialIdsInProject(projectId)
+	if err != nil {
+		return diag.Errorf("could not get cloud_credentials: %v", err)
+	}
+	found := false
+	for _, candidate := range credentialsList {
+		if candidate == credentialId {
+			found = true
+		}
+	}
+	if !found {
+		return diag.Errorf("could not find cloud credential project assignment.\n project id = %v, cloud credentials id = %v", projectId, credentialId)
+	}
+
+	d.SetId(getResourceId(credentialId, projectId))
+
+	return nil
+}
+
+func getResourceId(credentialId string, projectId string) string {
+	return credentialId + "|" + projectId
+}
+
+func resourceCloudCredentialsProjectAssignmentDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	apiClient := meta.(*client.ApiClient)
+
+	credential_id := d.Get("credential_id").(string)
+	projectId := d.Get("project_id").(string)
+	err := apiClient.RemoveCloudCredentialsFromProject(credential_id, projectId)
+	if err != nil {
+		return diag.Errorf("could not delete cloud credentials from project: %v", err)
+	}
+	return nil
+}
