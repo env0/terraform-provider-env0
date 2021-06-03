8d022769d4c2817f06dedcb6573c5e6a497eeacc
eranelbaz <eranelbaz97@gmail.com>
2021-06-03 16:14:58 +0300

    more

--- env0/resource_configuration_variable_test.go
+++ env0/resource_configuration_variable_test.go
@@ -1,6 +1,7 @@
 package env0
 
 import (
+	"errors"
 	"github.com/env0/terraform-provider-env0/client"
 	"github.com/golang/mock/gomock"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
@@ -44,7 +45,6 @@ func TestUnitConfigurationVariableResourceCreate(t *testing.T) {
 func TestUnitConfigurationVariableResourceCreateWrongType(t *testing.T) {
 	resourceType := "env0_configuration_variable"
 	resourceName := "test"
-	accessor := resourceAccessor(resourceType, resourceName)
 	configVar := client.ConfigurationVariable{
 		Id:    "id0",
 		Name:  "name0",
@@ -60,11 +60,6 @@ func TestUnitConfigurationVariableResourceCreateWrongType(t *testing.T) {
 					"value": configVar.Value,
 					"type":  configVar.Type,
 				}),
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr(accessor, "id", configVar.Id),
-					resource.TestCheckResourceAttr(accessor, "name", configVar.Name),
-					resource.TestCheckResourceAttr(accessor, "value", configVar.Value),
-				),
 				ExpectError: regexp.MustCompile(`(Error: 'type' can only receive either 'environment' or 'terraform')`),
 			},
 		},
@@ -72,6 +67,32 @@ func TestUnitConfigurationVariableResourceCreateWrongType(t *testing.T) {
 
 	runUnitTest(t, createTestCase, func(mock *client.MockApiClientInterface) {
 
+	})
+}
+func TestUnitConfigurationVariableResourceCreateApiClientError(t *testing.T) {
+	resourceType := "env0_configuration_variable"
+	resourceName := "test"
+	configVar := client.ConfigurationVariable{
+		Id:    "id0",
+		Name:  "name0",
+		Value: "Variable",
+	}
+
+	createTestCase := resource.TestCase{
+		Steps: []resource.TestStep{
+			{
+				Config: resourceConfigCreate(resourceType, resourceName, map[string]interface{}{
+					"name":  configVar.Name,
+					"value": configVar.Value,
+				}),
+				ExpectError: regexp.MustCompile(`(could not create configurationVariable: error)`),
+			},
+		},
+	}
+
+	runUnitTest(t, createTestCase, func(mock *client.MockApiClientInterface) {
+		mock.EXPECT().ConfigurationVariableCreate(configVar.Name, configVar.Value, false, client.ScopeGlobal, "", client.ConfigurationVariableTypeEnvironment,
+			nil).Times(1).Return(client.ConfigurationVariable{}, errors.New("error"))
 	})
 }
 func TestUnitConfigurationVariableResourceUpdate(t *testing.T) {
@@ -120,12 +141,58 @@ func TestUnitConfigurationVariableResourceUpdate(t *testing.T) {
 		mock.EXPECT().ConfigurationVariableCreate(configVar.Name, configVar.Value, false, client.ScopeGlobal, "", client.ConfigurationVariableTypeEnvironment,
 			nil).Times(1).Return(configVar, nil)
 		gomock.InOrder(
-			mock.EXPECT().ConfigurationVariables(client.ScopeGlobal, "").Return([]client.ConfigurationVariable{configVar}, nil),
-			mock.EXPECT().ConfigurationVariables(client.ScopeGlobal, "").Return([]client.ConfigurationVariable{configVar}, nil),
+			mock.EXPECT().ConfigurationVariables(client.ScopeGlobal, "").Return([]client.ConfigurationVariable{configVar}, nil).Times(2),
 			mock.EXPECT().ConfigurationVariables(client.ScopeGlobal, "").Return([]client.ConfigurationVariable{newConfigVar}, nil),
 		)
 		mock.EXPECT().ConfigurationVariableUpdate(newConfigVar.Id, newConfigVar.Name, newConfigVar.Value, false, client.ScopeGlobal, "", client.ConfigurationVariableTypeEnvironment,
 			nil).Times(1).Return(configVar, nil)
+		mock.EXPECT().ConfigurationVariableDelete(configVar.Id).Times(1).Return(nil)
+	})
+}
+func TestUnitConfigurationVariableResourceUpdateWrongType(t *testing.T) {
+	resourceType := "env0_configuration_variable"
+	resourceName := "test"
+	accessor := resourceAccessor(resourceType, resourceName)
+	configVar := client.ConfigurationVariable{
+		Id:    "id0",
+		Name:  "name0",
+		Value: "Variable",
+	}
+	newConfigVar := client.ConfigurationVariable{
+		Id:    configVar.Id,
+		Name:  configVar.Name,
+		Value: "I want to be the config value",
+		Type:  6,
+	}
+
+	updateTestCase := resource.TestCase{
+		Steps: []resource.TestStep{
+			{
+				Config: resourceConfigCreate(resourceType, resourceName, map[string]interface{}{
+					"name":  configVar.Name,
+					"value": configVar.Value,
+				}),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(accessor, "id", configVar.Id),
+					resource.TestCheckResourceAttr(accessor, "name", configVar.Name),
+					resource.TestCheckResourceAttr(accessor, "value", configVar.Value),
+				),
+			},
+			{
+				Config: resourceConfigCreate(resourceType, resourceName, map[string]interface{}{
+					"name":  newConfigVar.Name,
+					"value": newConfigVar.Value,
+					"type":  newConfigVar.Type,
+				}),
+				ExpectError: regexp.MustCompile(`'type' can only receive either 'environment' or 'terraform'`),
+			},
+		},
+	}
+
+	runUnitTest(t, updateTestCase, func(mock *client.MockApiClientInterface) {
+		mock.EXPECT().ConfigurationVariableCreate(configVar.Name, configVar.Value, false, client.ScopeGlobal, "", client.ConfigurationVariableTypeEnvironment,
+			nil).Times(1).Return(configVar, nil)
+		mock.EXPECT().ConfigurationVariables(client.ScopeGlobal, "").Return([]client.ConfigurationVariable{configVar}, nil).Times(2)
 		mock.EXPECT().ConfigurationVariableDelete(configVar.Id).Times(1).Return(nil)
 
 	})
