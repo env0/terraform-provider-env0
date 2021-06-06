35233e15a653a1aba41424c3c56375833f75d9b8
avner.sorek <avner.sorek@env0.com>
2021-06-06 10:43:00 +0300

    PR refactor

--- env0/data_project_test.go
+++ env0/data_project_test.go
@@ -7,54 +7,53 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
-var project = client.Project{
-	Id:          "id0",
-	Name:        "my-project-1",
-	CreatedBy:   "env0",
-	Role:        "role0",
-	Description: "A project's description",
-}
-
-func testProjectDataSource(t *testing.T, input map[string]string, mockFunc func(mockFunc *client.MockApiClientInterface)) {
-	resourceType := "env0_project"
-	resourceName := "test_project"
-	resourceFullName := dataSourceAccessor(resourceType, resourceName)
-
-	testCase := resource.TestCase{
-		Steps: []resource.TestStep{
-			{
-				Config: dataSourceConfigCreate(resourceType, resourceName, input),
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr(resourceFullName, "id", project.Id),
-					resource.TestCheckResourceAttr(resourceFullName, "name", project.Name),
-					resource.TestCheckResourceAttr(resourceFullName, "created_by", project.CreatedBy),
-					resource.TestCheckResourceAttr(resourceFullName, "role", project.Role),
-					resource.TestCheckResourceAttr(resourceFullName, "description", project.Description),
-				),
-			},
-		},
+func TestProjectDataSource(t *testing.T) {
+	project := client.Project{
+		Id:          "id0",
+		Name:        "my-project-1",
+		CreatedBy:   "env0",
+		Role:        "role0",
+		Description: "A project's description",
 	}
 
-	runUnitTest(t, testCase, mockFunc)
-}
+	testProjectDataSource := func(input map[string]string, mockFunc func(mockFunc *client.MockApiClientInterface)) {
+		resourceType := "env0_project"
+		resourceName := "test_project"
+		accessor := dataSourceAccessor(resourceType, resourceName)
 
-func TestUnitProjectDataByName(t *testing.T) {
-	testProjectDataSource(
-		t,
-		map[string]string{"name": project.Name},
-		func(mock *client.MockApiClientInterface) {
-			mock.EXPECT().Projects().AnyTimes().Return([]client.Project{project}, nil)
-		},
-	)
+		testCase := resource.TestCase{
+			Steps: []resource.TestStep{
+				{
+					Config: dataSourceConfigCreate(resourceType, resourceName, input),
+					Check: resource.ComposeAggregateTestCheckFunc(
+						resource.TestCheckResourceAttr(accessor, "id", project.Id),
+						resource.TestCheckResourceAttr(accessor, "name", project.Name),
+						resource.TestCheckResourceAttr(accessor, "created_by", project.CreatedBy),
+						resource.TestCheckResourceAttr(accessor, "role", project.Role),
+						resource.TestCheckResourceAttr(accessor, "description", project.Description),
+					),
+				},
+			},
+		}
 
-}
+		runUnitTest(t, testCase, mockFunc)
+	}
 
-func TestUnitProjectDataById(t *testing.T) {
-	testProjectDataSource(
-		t,
-		map[string]string{"id": project.Id},
-		func(mock *client.MockApiClientInterface) {
-			mock.EXPECT().Project(project.Id).AnyTimes().Return(project, nil)
-		},
-	)
+	t.Run("By ID", func(t *testing.T) {
+		testProjectDataSource(
+			map[string]string{"id": project.Id},
+			func(mock *client.MockApiClientInterface) {
+				mock.EXPECT().Project(project.Id).AnyTimes().Return(project, nil)
+			},
+		)
+	})
+
+	t.Run("By Name", func(t *testing.T) {
+		testProjectDataSource(
+			map[string]string{"name": project.Name},
+			func(mock *client.MockApiClientInterface) {
+				mock.EXPECT().Projects().AnyTimes().Return([]client.Project{project}, nil)
+			},
+		)
+	})
 }
