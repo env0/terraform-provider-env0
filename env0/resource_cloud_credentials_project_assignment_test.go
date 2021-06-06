105f1f855b72798fc4e46545a79546468f32c02a
eranelbaz <eranelbaz97@gmail.com>
2021-06-06 16:44:11 +0300

    create ut

---
+++ env0/resource_cloud_credentials_project_assignment_test.go
@@ -0,0 +1,39 @@
+package env0
+
+import (
+	"github.com/env0/terraform-provider-env0/client"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"testing"
+)
+
+func TestUnitResourceCloudCredentialsProjectAssignmentResource_Create(t *testing.T) {
+	resourceType := "env0_cloud_credentials_project_assignment"
+	resourceName := "test"
+	accessor := resourceAccessor(resourceType, resourceName)
+	assignment := client.CloudCredentialsProjectAssignment{
+		CredentialId: "cred-it",
+		ProjectId:    "proj-it",
+	}
+
+	createTestCase := resource.TestCase{
+		Steps: []resource.TestStep{
+			{
+				Config: resourceConfigCreate(resourceType, resourceName, map[string]interface{}{
+					"credential_id": assignment.CredentialId,
+					"project_id":    assignment.ProjectId,
+				}),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(accessor, "id", assignment.CredentialId+"|"+assignment.ProjectId),
+					resource.TestCheckResourceAttr(accessor, "credential_id", assignment.CredentialId),
+					resource.TestCheckResourceAttr(accessor, "project_id", assignment.ProjectId),
+				),
+			},
+		},
+	}
+
+	runUnitTest(t, createTestCase, func(mock *client.MockApiClientInterface) {
+		mock.EXPECT().AssignCloudCredentialsToProject(assignment.ProjectId, assignment.CredentialId).Times(1).Return(assignment, nil)
+		mock.EXPECT().CloudCredentialIdsInProject(assignment.ProjectId).Times(1).Return([]string{assignment.CredentialId}, nil)
+		mock.EXPECT().RemoveCloudCredentialsFromProject(assignment.ProjectId, assignment.CredentialId).Times(1).Return(nil)
+	})
+}
