62b2c69a8cd688b5ed8892c56b38f4a8f6b647e4
eranelbaz <eranelbaz97@gmail.com>
2021-06-06 17:28:18 +0300

    update to @avner method

--- env0/resource_cloud_credentials_project_assignment_test.go
+++ env0/resource_cloud_credentials_project_assignment_test.go
@@ -8,7 +8,7 @@ import (
 	"testing"
 )
 
-func TestUnitResourceCloudCredentialsProjectAssignmentResource_Create(t *testing.T) {
+func TestUnitResourceCloudCredentialsProjectAssignmentResource(t *testing.T) {
 	resourceType := "env0_cloud_credentials_project_assignment"
 	resourceName := "test"
 	accessor := resourceAccessor(resourceType, resourceName)
@@ -17,132 +17,105 @@ func TestUnitResourceCloudCredentialsProjectAssignmentResource_Create(t *testing
 		ProjectId:    "proj-it",
 	}
 
-	createTestCase := resource.TestCase{
-		Steps: []resource.TestStep{
-			{
-				Config: resourceConfigCreate(resourceType, resourceName, map[string]interface{}{
-					"credential_id": assignment.CredentialId,
-					"project_id":    assignment.ProjectId,
-				}),
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr(accessor, "id", assignment.CredentialId+"|"+assignment.ProjectId),
-					resource.TestCheckResourceAttr(accessor, "credential_id", assignment.CredentialId),
-					resource.TestCheckResourceAttr(accessor, "project_id", assignment.ProjectId),
-				),
+	t.Run("Create", func(t *testing.T) {
+		testCase := resource.TestCase{
+			Steps: []resource.TestStep{
+				{
+					Config: resourceConfigCreate(resourceType, resourceName, map[string]interface{}{
+						"credential_id": assignment.CredentialId,
+						"project_id":    assignment.ProjectId,
+					}),
+					Check: resource.ComposeAggregateTestCheckFunc(
+						resource.TestCheckResourceAttr(accessor, "id", assignment.CredentialId+"|"+assignment.ProjectId),
+						resource.TestCheckResourceAttr(accessor, "credential_id", assignment.CredentialId),
+						resource.TestCheckResourceAttr(accessor, "project_id", assignment.ProjectId),
+					),
+				},
 			},
-		},
-	}
+		}
 
-	runUnitTest(t, createTestCase, func(mock *client.MockApiClientInterface) {
-		mock.EXPECT().AssignCloudCredentialsToProject(assignment.ProjectId, assignment.CredentialId).Times(1).Return(assignment, nil)
-		mock.EXPECT().CloudCredentialIdsInProject(assignment.ProjectId).Times(1).Return([]string{assignment.CredentialId}, nil)
-		mock.EXPECT().RemoveCloudCredentialsFromProject(assignment.ProjectId, assignment.CredentialId).Times(1).Return(nil)
+		runUnitTest(t, testCase, func(mock *client.MockApiClientInterface) {
+			mock.EXPECT().AssignCloudCredentialsToProject(assignment.ProjectId, assignment.CredentialId).Times(1).Return(assignment, nil)
+			mock.EXPECT().CloudCredentialIdsInProject(assignment.ProjectId).Times(1).Return([]string{assignment.CredentialId}, nil)
+			mock.EXPECT().RemoveCloudCredentialsFromProject(assignment.ProjectId, assignment.CredentialId).Times(1).Return(nil)
+		})
 	})
-}
-func TestUnitResourceCloudCredentialsProjectAssignmentResource_CreateApiProb(t *testing.T) {
-	resourceType := "env0_cloud_credentials_project_assignment"
-	resourceName := "test"
-	assignment := client.CloudCredentialsProjectAssignment{
-		CredentialId: "cred-it",
-		ProjectId:    "proj-it",
-	}
-
-	createTestCase := resource.TestCase{
-		Steps: []resource.TestStep{
-			{
-				Config: resourceConfigCreate(resourceType, resourceName, map[string]interface{}{
-					"credential_id": assignment.CredentialId,
-					"project_id":    assignment.ProjectId,
-				}),
-				ExpectError: regexp.MustCompile(`(could not assign cloud credentials to project)`),
+	t.Run("Create with api prob", func(t *testing.T) {
+		testCase := resource.TestCase{
+			Steps: []resource.TestStep{
+				{
+					Config: resourceConfigCreate(resourceType, resourceName, map[string]interface{}{
+						"credential_id": assignment.CredentialId,
+						"project_id":    assignment.ProjectId,
+					}),
+					ExpectError: regexp.MustCompile(`(could not assign cloud credentials to project)`),
+				},
 			},
-		},
-	}
+		}
 
-	runUnitTest(t, createTestCase, func(mock *client.MockApiClientInterface) {
-		mock.EXPECT().AssignCloudCredentialsToProject(assignment.ProjectId, assignment.CredentialId).Times(1).Return(client.CloudCredentialsProjectAssignment{}, errors.New("err"))
+		runUnitTest(t, testCase, func(mock *client.MockApiClientInterface) {
+			mock.EXPECT().AssignCloudCredentialsToProject(assignment.ProjectId, assignment.CredentialId).Times(1).Return(client.CloudCredentialsProjectAssignment{}, errors.New("err"))
+		})
 	})
-}
-func TestUnitResourceCloudCredentialsProjectAssignmentResource_ReadApiProb(t *testing.T) {
-	resourceType := "env0_cloud_credentials_project_assignment"
-	resourceName := "test"
-	assignment := client.CloudCredentialsProjectAssignment{
-		CredentialId: "cred-it",
-		ProjectId:    "proj-it",
-	}
-
-	createTestCase := resource.TestCase{
-		Steps: []resource.TestStep{
-			{
-				Config: resourceConfigCreate(resourceType, resourceName, map[string]interface{}{
-					"credential_id": assignment.CredentialId,
-					"project_id":    assignment.ProjectId,
-				}),
-				ExpectError: regexp.MustCompile(`(could not get cloud_credentials:)`),
+	t.Run("Read with api prob", func(t *testing.T) {
+		testCase := resource.TestCase{
+			Steps: []resource.TestStep{
+				{
+					Config: resourceConfigCreate(resourceType, resourceName, map[string]interface{}{
+						"credential_id": assignment.CredentialId,
+						"project_id":    assignment.ProjectId,
+					}),
+					ExpectError: regexp.MustCompile(`(could not get cloud_credentials:)`),
+				},
 			},
-		},
-	}
+		}
 
-	runUnitTest(t, createTestCase, func(mock *client.MockApiClientInterface) {
-		mock.EXPECT().AssignCloudCredentialsToProject(assignment.ProjectId, assignment.CredentialId).Times(1).Return(assignment, nil)
-		mock.EXPECT().CloudCredentialIdsInProject(assignment.ProjectId).Times(1).Return([]string{}, errors.New("err"))
-		mock.EXPECT().RemoveCloudCredentialsFromProject(assignment.ProjectId, assignment.CredentialId).Times(1).Return(nil)
+		runUnitTest(t, testCase, func(mock *client.MockApiClientInterface) {
+			mock.EXPECT().AssignCloudCredentialsToProject(assignment.ProjectId, assignment.CredentialId).Times(1).Return(assignment, nil)
+			mock.EXPECT().CloudCredentialIdsInProject(assignment.ProjectId).Times(1).Return([]string{}, errors.New("err"))
+			mock.EXPECT().RemoveCloudCredentialsFromProject(assignment.ProjectId, assignment.CredentialId).Times(1).Return(nil)
+		})
 	})
-}
-func TestUnitResourceCloudCredentialsProjectAssignmentResource_ReadMultiValues(t *testing.T) {
-	resourceType := "env0_cloud_credentials_project_assignment"
-	resourceName := "test"
-	accessor := resourceAccessor(resourceType, resourceName)
-	assignment := client.CloudCredentialsProjectAssignment{
-		CredentialId: "cred-it",
-		ProjectId:    "proj-it",
-	}
-
-	createTestCase := resource.TestCase{
-		Steps: []resource.TestStep{
-			{
-				Config: resourceConfigCreate(resourceType, resourceName, map[string]interface{}{
-					"credential_id": assignment.CredentialId,
-					"project_id":    assignment.ProjectId,
-				}),
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr(accessor, "id", assignment.CredentialId+"|"+assignment.ProjectId),
-					resource.TestCheckResourceAttr(accessor, "credential_id", assignment.CredentialId),
-					resource.TestCheckResourceAttr(accessor, "project_id", assignment.ProjectId),
-				),
+	t.Run("Read with multi values", func(t *testing.T) {
+		testCase := resource.TestCase{
+			Steps: []resource.TestStep{
+				{
+					Config: resourceConfigCreate(resourceType, resourceName, map[string]interface{}{
+						"credential_id": assignment.CredentialId,
+						"project_id":    assignment.ProjectId,
+					}),
+					Check: resource.ComposeAggregateTestCheckFunc(
+						resource.TestCheckResourceAttr(accessor, "id", assignment.CredentialId+"|"+assignment.ProjectId),
+						resource.TestCheckResourceAttr(accessor, "credential_id", assignment.CredentialId),
+						resource.TestCheckResourceAttr(accessor, "project_id", assignment.ProjectId),
+					),
+				},
 			},
-		},
-	}
+		}
 
-	runUnitTest(t, createTestCase, func(mock *client.MockApiClientInterface) {
-		mock.EXPECT().AssignCloudCredentialsToProject(assignment.ProjectId, assignment.CredentialId).Times(1).Return(assignment, nil)
-		mock.EXPECT().CloudCredentialIdsInProject(assignment.ProjectId).Times(1).Return([]string{assignment.CredentialId, "1", "2"}, nil)
-		mock.EXPECT().RemoveCloudCredentialsFromProject(assignment.ProjectId, assignment.CredentialId).Times(1).Return(nil)
+		runUnitTest(t, testCase, func(mock *client.MockApiClientInterface) {
+			mock.EXPECT().AssignCloudCredentialsToProject(assignment.ProjectId, assignment.CredentialId).Times(1).Return(assignment, nil)
+			mock.EXPECT().CloudCredentialIdsInProject(assignment.ProjectId).Times(1).Return([]string{assignment.CredentialId, "1", "2"}, nil)
+			mock.EXPECT().RemoveCloudCredentialsFromProject(assignment.ProjectId, assignment.CredentialId).Times(1).Return(nil)
+		})
 	})
-}
-func TestUnitResourceCloudCredentialsProjectAssignmentResource_ReadDidntReturnCorrect(t *testing.T) {
-	resourceType := "env0_cloud_credentials_project_assignment"
-	resourceName := "test"
-	assignment := client.CloudCredentialsProjectAssignment{
-		CredentialId: "cred-it",
-		ProjectId:    "proj-it",
-	}
-
-	createTestCase := resource.TestCase{
-		Steps: []resource.TestStep{
-			{
-				Config: resourceConfigCreate(resourceType, resourceName, map[string]interface{}{
-					"credential_id": assignment.CredentialId,
-					"project_id":    assignment.ProjectId,
-				}),
-				ExpectError: regexp.MustCompile(`(could not find cloud credential project assignment)`),
+	t.Run("Read didnt api didnt return correct stuff", func(t *testing.T) {
+		testCase := resource.TestCase{
+			Steps: []resource.TestStep{
+				{
+					Config: resourceConfigCreate(resourceType, resourceName, map[string]interface{}{
+						"credential_id": assignment.CredentialId,
+						"project_id":    assignment.ProjectId,
+					}),
+					ExpectError: regexp.MustCompile(`(could not find cloud credential project assignment)`),
+				},
 			},
-		},
-	}
+		}
 
-	runUnitTest(t, createTestCase, func(mock *client.MockApiClientInterface) {
-		mock.EXPECT().AssignCloudCredentialsToProject(assignment.ProjectId, assignment.CredentialId).Times(1).Return(assignment, nil)
-		mock.EXPECT().CloudCredentialIdsInProject(assignment.ProjectId).Times(1).Return([]string{"1", "2"}, nil)
-		mock.EXPECT().RemoveCloudCredentialsFromProject(assignment.ProjectId, assignment.CredentialId).Times(1).Return(nil)
+		runUnitTest(t, testCase, func(mock *client.MockApiClientInterface) {
+			mock.EXPECT().AssignCloudCredentialsToProject(assignment.ProjectId, assignment.CredentialId).Times(1).Return(assignment, nil)
+			mock.EXPECT().CloudCredentialIdsInProject(assignment.ProjectId).Times(1).Return([]string{"1", "2"}, nil)
+			mock.EXPECT().RemoveCloudCredentialsFromProject(assignment.ProjectId, assignment.CredentialId).Times(1).Return(nil)
+		})
 	})
 }
