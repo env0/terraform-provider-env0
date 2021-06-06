a78b71eb4bc7138571fa96920cebd33c0785b617
eranelbaz <eranelbaz97@gmail.com>
2021-06-06 16:57:03 +0300

    Added api prob create

--- env0/resource_cloud_credentials_project_assignment_test.go
+++ env0/resource_cloud_credentials_project_assignment_test.go
@@ -1,8 +1,10 @@
 package env0
 
 import (
+	"errors"
 	"github.com/env0/terraform-provider-env0/client"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"regexp"
 	"testing"
 )
 
@@ -35,5 +37,29 @@ func TestUnitResourceCloudCredentialsProjectAssignmentResource_Create(t *testing
 		mock.EXPECT().AssignCloudCredentialsToProject(assignment.ProjectId, assignment.CredentialId).Times(1).Return(assignment, nil)
 		mock.EXPECT().CloudCredentialIdsInProject(assignment.ProjectId).Times(1).Return([]string{assignment.CredentialId}, nil)
 		mock.EXPECT().RemoveCloudCredentialsFromProject(assignment.ProjectId, assignment.CredentialId).Times(1).Return(nil)
+	})
+}
+func TestUnitResourceCloudCredentialsProjectAssignmentResource_CreateApiProb(t *testing.T) {
+	resourceType := "env0_cloud_credentials_project_assignment"
+	resourceName := "test"
+	assignment := client.CloudCredentialsProjectAssignment{
+		CredentialId: "cred-it",
+		ProjectId:    "proj-it",
+	}
+
+	createTestCase := resource.TestCase{
+		Steps: []resource.TestStep{
+			{
+				Config: resourceConfigCreate(resourceType, resourceName, map[string]interface{}{
+					"credential_id": assignment.CredentialId,
+					"project_id":    assignment.ProjectId,
+				}),
+				ExpectError: regexp.MustCompile(`(could not assign cloud credentials to project)`),
+			},
+		},
+	}
+
+	runUnitTest(t, createTestCase, func(mock *client.MockApiClientInterface) {
+		mock.EXPECT().AssignCloudCredentialsToProject(assignment.ProjectId, assignment.CredentialId).Times(1).Return(client.CloudCredentialsProjectAssignment{}, errors.New("err"))
 	})
 }
