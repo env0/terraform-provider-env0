52582eacbd35c7df5278084535c15694491dae08
Yaron Yarimi <yaron.yarimi@env0.com>
2021-06-06 15:11:38 +0300

    add version to main serve provider

--- main.go
+++ main.go
@@ -1,12 +1,35 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"github.com/env0/terraform-provider-env0/env0"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+	"log"
+)
+
+//go:generate terraform fmt -recursive ./examples/
+
+var (
+	// these will be set by the goreleaser configuration
+	// to appropriate values for the compiled binary
+	version string = "dev"
 )
 
 func main() {
-	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: env0.Provider,
-	})
+	var debugMode bool
+	flag.BoolVar(&debugMode, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
+	opts := &plugin.ServeOpts{ProviderFunc: env0.Provider(version)}
+
+	if debugMode {
+		err := plugin.Debug(context.Background(), "registry.terraform.io/env0/env0", opts)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		return
+	}
+
+	plugin.Serve(opts)
 }
