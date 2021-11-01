44daa9197e58026282973d24cd85918306303858
eranelbaz <eranelbaz97@gmail.com>
2021-11-01 15:26:58 +0200

    added description

--- examples/resources/env0_configuration_variable/resource.tf
+++ examples/resources/env0_configuration_variable/resource.tf
@@ -1,6 +1,7 @@
 resource "env0_configuration_variable" "example" {
-  name  = "ENVIRONMENT_VARIABLE_NAME"
-  value = "example value"
+  name        = "ENVIRONMENT_VARIABLE_NAME"
+  value       = "example value"
+  description = "Here you can fill description for this variable"
 }
 
 resource "env0_configuration_variable" "drop_down" {
