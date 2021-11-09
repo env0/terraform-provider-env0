5177f212842a7e8d9a8433d7a2532b814ad29a01
Adam Bernot <adam.bernot@toptal.com>
2021-11-09 09:44:30 -0500

    test: fix resource name in integration test

--- tests/integration/011_policy/main.tf
+++ tests/integration/011_policy/main.tf
@@ -2,12 +2,12 @@ data "env0_project" "default" {
   name = "Default Organization Project"
 }
 
-resource "env0_policy" "test_policy" {
+resource "env0_project_policy" "test_policy" {
   project_id                   = data.env0_project.default.id
   number_of_environments_total = 1
 }
 
-resource "env0_policy" "test_policy_2" {
+resource "env0_project_policy" "test_policy_2" {
   project_id                   = data.env0_project.default.id
   number_of_environments_total = 0
 }
