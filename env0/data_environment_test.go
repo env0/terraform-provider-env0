eb4ebac33090886aaf30f05f823139b1b3024c3b
eranelbaz <eranelbaz97@gmail.com>
2021-12-13 12:44:09 +0200

    fix all usecases

--- env0/data_environment_test.go
+++ env0/data_environment_test.go
@@ -2,6 +2,7 @@ package env0
 
 import (
 	"regexp"
+	"strconv"
 	"testing"
 
 	"github.com/env0/terraform-provider-env0/client"
@@ -9,12 +10,17 @@ import (
 )
 
 func TestEnvironmentDataSource(t *testing.T) {
+	boolean := true
 	environment := client.Environment{
-		Id:                    "id-0",
-		Name:                  "my-environment-1",
-		ProjectId:             "project-id",
-		Status:                "status",
-		LatestDeploymentLogId: "latest-deployment-log-id",
+		Id:                          "id-0",
+		Name:                        "my-environment-1",
+		ProjectId:                   "project-id",
+		Status:                      "status",
+		LatestDeploymentLogId:       "latest-deployment-log-id",
+		RequiresApproval:            &boolean,
+		PullRequestPlanDeployments:  &boolean,
+		AutoDeployOnPathChangesOnly: &boolean,
+		ContinuousDeployment:        &boolean,
 		LatestDeploymentLog: client.DeploymentLog{
 			BlueprintId:       "blueprint-id",
 			BlueprintRevision: "revision",
@@ -42,10 +48,10 @@ func TestEnvironmentDataSource(t *testing.T) {
 						resource.TestCheckResourceAttr(accessor, "id", environment.Id),
 						resource.TestCheckResourceAttr(accessor, "name", environment.Name),
 						resource.TestCheckResourceAttr(accessor, "project_id", environment.ProjectId),
-						//resource.TestCheckResourceAttr(accessor, "approve_plan_automatically", strconv.FormatBool(*environment.RequiresApproval)),
-						//resource.TestCheckResourceAttr(accessor, "run_plan_on_pull_requests", strconv.FormatBool(*environment.PullRequestPlanDeployments)),
-						//resource.TestCheckResourceAttr(accessor, "auto_deploy_on_path_changes_only", strconv.FormatBool(*environment.AutoDeployOnPathChangesOnly)),
-						//resource.TestCheckResourceAttr(accessor, "deploy_on_push", strconv.FormatBool(*environment.ContinuousDeployment)),
+						resource.TestCheckResourceAttr(accessor, "approve_plan_automatically", strconv.FormatBool(!*environment.RequiresApproval)),
+						resource.TestCheckResourceAttr(accessor, "run_plan_on_pull_requests", strconv.FormatBool(*environment.PullRequestPlanDeployments)),
+						resource.TestCheckResourceAttr(accessor, "auto_deploy_on_path_changes_only", strconv.FormatBool(*environment.AutoDeployOnPathChangesOnly)),
+						resource.TestCheckResourceAttr(accessor, "deploy_on_push", strconv.FormatBool(*environment.ContinuousDeployment)),
 						resource.TestCheckResourceAttr(accessor, "status", environment.Status),
 						resource.TestCheckResourceAttr(accessor, "latest_deployment_log_id", environment.LatestDeploymentLogId),
 						resource.TestCheckResourceAttr(accessor, "template_id", environment.LatestDeploymentLog.BlueprintId),
