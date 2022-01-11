32fded71f4ff7b022353c4f41ae77949a4b95c56
eladmoshe <elad.moshe@env0.com>
2022-01-11 16:13:40 +0200

    adding tests

--- env0/resource_configuration_variable_test.go
+++ env0/resource_configuration_variable_test.go
@@ -3,27 +3,35 @@ package env0
 import (
 	"errors"
 	"fmt"
+	"regexp"
+	"strconv"
+	"testing"
+
 	"github.com/env0/terraform-provider-env0/client"
 	"github.com/golang/mock/gomock"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"regexp"
-	"testing"
 )
 
 func TestUnitConfigurationVariableResource(t *testing.T) {
 	resourceType := "env0_configuration_variable"
 	resourceName := "test"
+	isReadonly := true
+	isRequired := false
 	accessor := resourceAccessor(resourceType, resourceName)
 	configVar := client.ConfigurationVariable{
 		Id:          "id0",
 		Name:        "name0",
 		Description: "desc0",
 		Value:       "Variable",
+		IsReadonly:  &isReadonly,
+		IsRequired:  &isRequired,
 	}
 	stepConfig := resourceConfigCreate(resourceType, resourceName, map[string]interface{}{
-		"name":        configVar.Name,
-		"description": configVar.Description,
-		"value":       configVar.Value,
+		"name":         configVar.Name,
+		"description":  configVar.Description,
+		"value":        configVar.Value,
+		"is_read_only": strconv.FormatBool(*configVar.IsReadonly),
+		"is_required":  strconv.FormatBool(*configVar.IsRequired),
 	})
 
 	configurationVariableCreateParams := client.ConfigurationVariableCreateParams{
@@ -36,6 +44,8 @@ func TestUnitConfigurationVariableResource(t *testing.T) {
 		EnumValues:  nil,
 		Description: configVar.Description,
 		Format:      client.Text,
+		IsRequired:  *configVar.IsRequired,
+		IsReadonly:  *configVar.IsReadonly,
 	}
 	t.Run("Create", func(t *testing.T) {
 
@@ -48,6 +58,8 @@ func TestUnitConfigurationVariableResource(t *testing.T) {
 						resource.TestCheckResourceAttr(accessor, "name", configVar.Name),
 						resource.TestCheckResourceAttr(accessor, "description", configVar.Description),
 						resource.TestCheckResourceAttr(accessor, "value", configVar.Value),
+						resource.TestCheckResourceAttr(accessor, "is_read_only", strconv.FormatBool(*configVar.IsReadonly)),
+						resource.TestCheckResourceAttr(accessor, "is_required", strconv.FormatBool(*configVar.IsRequired)),
 					),
 				},
 			},
@@ -289,23 +301,29 @@ resource "%s" "test" {
 			Steps: []resource.TestStep{
 				{
 					Config: resourceConfigCreate(resourceType, resourceName, map[string]interface{}{
-						"name":        configVar.Name,
-						"description": configVar.Description,
-						"value":       configVar.Value,
+						"name":         configVar.Name,
+						"description":  configVar.Description,
+						"value":        configVar.Value,
+						"is_read_only": strconv.FormatBool(*configVar.IsReadonly),
+						"is_required":  strconv.FormatBool(*configVar.IsRequired),
 					}),
 					Check: resource.ComposeAggregateTestCheckFunc(
 						resource.TestCheckResourceAttr(accessor, "id", configVar.Id),
 						resource.TestCheckResourceAttr(accessor, "description", configVar.Description),
 						resource.TestCheckResourceAttr(accessor, "name", configVar.Name),
 						resource.TestCheckResourceAttr(accessor, "value", configVar.Value),
+						resource.TestCheckResourceAttr(accessor, "is_read_only", strconv.FormatBool(*configVar.IsReadonly)),
+						resource.TestCheckResourceAttr(accessor, "is_required", strconv.FormatBool(*configVar.IsRequired)),
 					),
 				},
 				{
 					Config: resourceConfigCreate(resourceType, resourceName, map[string]interface{}{
-						"name":        newConfigVar.Name,
-						"description": newConfigVar.Description,
-						"value":       newConfigVar.Value,
-						"format":      client.HCL,
+						"name":         newConfigVar.Name,
+						"description":  newConfigVar.Description,
+						"value":        newConfigVar.Value,
+						"format":       client.HCL,
+						"is_read_only": strconv.FormatBool(*configVar.IsReadonly),
+						"is_required":  strconv.FormatBool(*configVar.IsRequired),
 					}),
 					Check: resource.ComposeAggregateTestCheckFunc(
 						resource.TestCheckResourceAttr(accessor, "id", newConfigVar.Id),
@@ -313,6 +331,8 @@ resource "%s" "test" {
 						resource.TestCheckResourceAttr(accessor, "description", newConfigVar.Description),
 						resource.TestCheckResourceAttr(accessor, "value", newConfigVar.Value),
 						resource.TestCheckResourceAttr(accessor, "format", string(client.HCL)),
+						resource.TestCheckResourceAttr(accessor, "is_read_only", strconv.FormatBool(*configVar.IsReadonly)),
+						resource.TestCheckResourceAttr(accessor, "is_required", strconv.FormatBool(*configVar.IsRequired)),
 					),
 				},
 			},
@@ -349,9 +369,11 @@ resource "%s" "test" {
 			Steps: []resource.TestStep{
 				{
 					Config: resourceConfigCreate(resourceType, resourceName, map[string]interface{}{
-						"name":        configVar.Name,
-						"description": configVar.Description,
-						"value":       configVar.Value,
+						"name":         configVar.Name,
+						"description":  configVar.Description,
+						"value":        configVar.Value,
+						"is_read_only": strconv.FormatBool(*configVar.IsReadonly),
+						"is_required":  strconv.FormatBool(*configVar.IsRequired),
 					}),
 					Check: resource.ComposeAggregateTestCheckFunc(
 						resource.TestCheckResourceAttr(accessor, "id", configVar.Id),
@@ -366,6 +388,8 @@ resource "%s" "test" {
 						"description": newConfigVar.Description,
 						"value":       newConfigVar.Value,
 						"type":        newConfigVar.Type,
+						"is_read_only": strconv.FormatBool(*configVar.IsReadonly),
+						"is_required":  strconv.FormatBool(*configVar.IsRequired),
 					}),
 					ExpectError: regexp.MustCompile(`'type' can only receive either 'environment' or 'terraform'`),
 				},
