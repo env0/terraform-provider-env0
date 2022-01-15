04bed08a95443edb74503c04bf8159930aba78f8
SHMUELBRENER <SHMUELBRENER@GMAIL.COM>
2022-01-15 21:39:33 +0200

    add unit-test for data_aws_credential:by id,by name

---
+++ env0/data_aws_credentials_test.go
@@ -0,0 +1,63 @@
+package env0
+
+import (
+	"testing"
+
+	"github.com/env0/terraform-provider-env0/client"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAwsCredDataSource(t *testing.T) {
+	awsCred := client.ApiKey{
+		Id:   "11111",
+		Name: "testdata",
+	}
+
+	AwsCredFieldsByName := map[string]interface{}{"name": awsCred.Name}
+	AwsCredFieldsById := map[string]interface{}{"id": awsCred.Id}
+
+	resourceType := "env0_aws_credentials"
+	resourceName := "test_aws_cred"
+	accessor := dataSourceAccessor(resourceType, resourceName)
+
+	getValidTestCase := func(input map[string]interface{}) resource.TestCase {
+		return resource.TestCase{
+			Steps: []resource.TestStep{
+				{
+					Config: dataSourceConfigCreate(resourceType, resourceName, input),
+					Check: resource.ComposeAggregateTestCheckFunc(
+						resource.TestCheckResourceAttr(accessor, "id", awsCred.Id),
+						resource.TestCheckResourceAttr(accessor, "name", awsCred.Name),
+					),
+				},
+			},
+		}
+	}
+
+	mockGetAwsCredCall := func(returnValue client.ApiKey) func(mockFunc *client.MockApiClientInterface) {
+		return func(mock *client.MockApiClientInterface) {
+			mock.EXPECT().AwsCredentials(awsCred.Id).AnyTimes().Return(returnValue, nil)
+		}
+	}
+
+	mockListAwsCredCall := func(returnValue []client.ApiKey) func(mockFunc *client.MockApiClientInterface) {
+		return func(mock *client.MockApiClientInterface) {
+			mock.EXPECT().AwsCredentialsList().AnyTimes().Return(returnValue, nil)
+		}
+	}
+
+	t.Run("By ID", func(t *testing.T) {
+		runUnitTest(t,
+			getValidTestCase(AwsCredFieldsById),
+			mockGetAwsCredCall(awsCred),
+		)
+	})
+
+	t.Run("By Name", func(t *testing.T) {
+		runUnitTest(t,
+			getValidTestCase(AwsCredFieldsByName),
+			mockListAwsCredCall([]client.ApiKey{awsCred}),
+		)
+	})
+
+}
