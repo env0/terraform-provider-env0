4934b4df1c3d9c76e368271e500acd61f57194af
Yaron Yarimi <yaron.yarimi@env0.com>
2022-01-18 10:53:47 +0200

    add environment scheduling api calls

---
+++ client/environment_scheduling.go
@@ -0,0 +1,37 @@
+package client
+
+import "errors"
+
+func (self *ApiClient) EnvironmentScheduling(environmentId string) (EnvironmentScheduling, error) {
+	var result EnvironmentScheduling
+
+	err := self.http.Get("/scheduling/environments/"+environmentId, nil, &result)
+	if err != nil {
+		return result, err
+	}
+	return result, nil
+}
+
+func (self *ApiClient) EnvironmentSchedulingUpdate(environmentId string, payload EnvironmentScheduling) (EnvironmentScheduling, error) {
+	var result EnvironmentScheduling
+
+	if payload.Deploy.Cron == payload.Destroy.Cron {
+		return EnvironmentScheduling{}, errors.New("deploy and destroy cron expressions must not be the same")
+	}
+
+	err := self.http.Put("/scheduling/environments/"+environmentId, payload, &result)
+	if err != nil {
+		return EnvironmentScheduling{}, err
+	}
+
+	return result, nil
+}
+
+func (self *ApiClient) EnvironmentSchedulingDelete(environmentId string) error {
+	err := self.http.Put("/scheduling/environments/"+environmentId, EnvironmentSchedulingUpdatePayload{}, nil)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
