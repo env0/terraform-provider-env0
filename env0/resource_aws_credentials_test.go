8e9a2ffbddab6f2df491c8f7343e04d89a678d36
SHMUELBRENER <SHMUELBRENER@GMAIL.COM>
2022-01-18 17:01:16 +0200

    add DESTROY AND CREATE WHEN CHANGED-UT for resource_aws_credentilas

--- env0/resource_aws_credentials_test.go
+++ env0/resource_aws_credentials_test.go
@@ -4,7 +4,8 @@ import (
 	"testing"
 
 	"github.com/env0/terraform-provider-env0/client"
-	//	"github.com/golang/mock/gomock"
+
+	"github.com/golang/mock/gomock"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
@@ -28,6 +29,14 @@ func TestUnitAwsCredentialsResource(t *testing.T) {
 		},
 	}
 
+	updateAwsCredCreatePayload := client.AwsCredentialsCreatePayload{
+		Name: "update",
+		Value: client.AwsCredentialsValuePayload{
+			RoleArn:    awsCredentialResoure["arn"].(string),
+			ExternalId: awsCredentialResoure["external_id"].(string),
+		},
+	}
+
 	returnValues := client.ApiKey{
 		Id:             "id",
 		Name:           "test",
@@ -35,11 +44,18 @@ func TestUnitAwsCredentialsResource(t *testing.T) {
 		Type:           "AWS_ASSUMED_ROLE_FOR_DEPLOYMENT",
 	}
 
-	// updatedawsCredentialResoure := map[string]interface{}{
-	// 	"name":        "update",
-	// 	"arn":         "11111",
-	// 	"external_id": "22222",
-	// }
+	updateReturnValues := client.ApiKey{
+		Id:             "id2",
+		Name:           "update",
+		OrganizationId: "id",
+		Type:           "AWS_ASSUMED_ROLE_FOR_DEPLOYMENT",
+	}
+
+	updatedawsCredentialResoure := map[string]interface{}{
+		"name":        "update",
+		"arn":         "11111",
+		"external_id": "22222",
+	}
 
 	testCaseForCreate := resource.TestCase{
 		Steps: []resource.TestStep{
@@ -55,11 +71,48 @@ func TestUnitAwsCredentialsResource(t *testing.T) {
 		},
 	}
 
+	testCaseForUpdate := resource.TestCase{
+		Steps: []resource.TestStep{
+			{
+				Config: resourceConfigCreate(resourceType, resourceName, awsCredentialResoure),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(accessor, "name", awsCredentialResoure["name"].(string)),
+					resource.TestCheckResourceAttr(accessor, "arn", awsCredentialResoure["arn"].(string)),
+					resource.TestCheckResourceAttr(accessor, "external_id", awsCredentialResoure["external_id"].(string)),
+					resource.TestCheckResourceAttr(accessor, "id", returnValues.Id),
+				),
+			},
+			{
+				Config: resourceConfigCreate(resourceType, resourceName, updatedawsCredentialResoure),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(accessor, "name", updatedawsCredentialResoure["name"].(string)),
+					resource.TestCheckResourceAttr(accessor, "arn", updatedawsCredentialResoure["arn"].(string)),
+					resource.TestCheckResourceAttr(accessor, "external_id", updatedawsCredentialResoure["external_id"].(string)),
+					resource.TestCheckResourceAttr(accessor, "id", updateReturnValues.Id),
+				),
+			},
+		},
+	}
+
 	t.Run("create", func(t *testing.T) {
 		runUnitTest(t, testCaseForCreate, func(mock *client.MockApiClientInterface) {
 			mock.EXPECT().AwsCredentialsCreate(awsCredCreatePayload).Times(1).Return(returnValues, nil)
 			mock.EXPECT().AwsCredentials(returnValues.Id).Times(1).Return(returnValues, nil)
 			mock.EXPECT().AwsCredentialsDelete(returnValues.Id).Times(1).Return(nil)
+		})
+	})
+
+	t.Run("destroy when one of the values changed", func(t *testing.T) {
+		runUnitTest(t, testCaseForUpdate, func(mock *client.MockApiClientInterface) {
+			mock.EXPECT().AwsCredentialsCreate(awsCredCreatePayload).Times(1).Return(returnValues, nil)
+			mock.EXPECT().AwsCredentialsDelete(returnValues.Id).Times(1).Return(nil)
+			mock.EXPECT().AwsCredentialsCreate(updateAwsCredCreatePayload).Times(1).Return(updateReturnValues, nil)
+
+			gomock.InOrder(
+				mock.EXPECT().AwsCredentials(returnValues.Id).Times(2).Return(returnValues, nil),
+				mock.EXPECT().AwsCredentials(updateReturnValues.Id).Times(1).Return(updateReturnValues, nil),
+			)
+			mock.EXPECT().AwsCredentialsDelete(updateReturnValues.Id).Times(1).Return(nil)
 		})
 	})
 
