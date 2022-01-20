81f6e73252fb937cc5fd79436ea0652caf2b2213
Elbaz <eran.elbaz@env0.com>
2022-01-20 18:38:04 +0200

    update tests

--- client/workflow_triggers_test.go
+++ client/workflow_triggers_test.go
@@ -9,49 +9,50 @@ import (
 
 var _ = Describe("Workflow Triggers", func() {
 	const environmentId = "environmentId"
-	mockTrigger := WorkflowTrigger{
-		Id:            "id1",
-		Name:          "name",
-		WorkspaceName: "workspaceName",
-		ProjectId:     "projectId",
+	mockTrigger := []WorkflowTrigger{
+		{
+			Id:            "id1",
+			Name:          "name",
+			WorkspaceName: "workspaceName",
+			ProjectId:     "projectId",
+		},
 	}
 
-	Describe("WorkflowTriggerCreate", func() {
-		var trigger WorkflowTrigger
+	var triggers []WorkflowTrigger
+	Describe("WorkflowTriggerUpsert", func() {
 
 		BeforeEach(func() {
 			httpCall = mockHttpClient.EXPECT().
 				Put("/environments/"+environmentId+"/downstream", WorkflowTriggerUpsertPayload{
-					DownstreamEnvironmentIds: []string{mockTrigger.Id},
+					DownstreamEnvironmentIds: []string{mockTrigger[0].Id},
 				},
 					gomock.Any()).
-				Do(func(path string, request interface{}, response *WorkflowTrigger) {
+				Do(func(path string, request interface{}, response *[]WorkflowTrigger) {
 					*response = mockTrigger
 				})
 
-			trigger, _ = apiClient.WorkflowTriggerCreate(environmentId, WorkflowTriggerUpsertPayload{
-				DownstreamEnvironmentIds: []string{mockTrigger.Id},
+			triggers, _ = apiClient.WorkflowTriggerUpsert(environmentId, WorkflowTriggerUpsertPayload{
+				DownstreamEnvironmentIds: []string{mockTrigger[0].Id},
 			})
 		})
 
-		It("Should return created trigger", func() {
-			Expect(trigger).To(Equal(mockTrigger))
+		It("Should return created triggers", func() {
+			Expect(triggers).To(Equal(mockTrigger))
 		})
 	})
 
 	Describe("WorkflowTrigger", func() {
-		var queriedTriggers []WorkflowTrigger
 		BeforeEach(func() {
 			httpCall = mockHttpClient.EXPECT().
 				Get("environments/"+environmentId+"/downstream", nil, gomock.Any()).
 				Do(func(path string, request interface{}, response *[]WorkflowTrigger) {
-					*response = []WorkflowTrigger{mockTrigger}
+					*response = mockTrigger
 				})
-			queriedTriggers, _ = apiClient.WorkflowTrigger(environmentId)
+			triggers, _ = apiClient.WorkflowTrigger(environmentId)
 		})
 
 		It("Should return correct triggers", func() {
-			Expect(queriedTriggers).To(Equal([]WorkflowTrigger{mockTrigger}))
+			Expect(triggers).To(Equal(mockTrigger))
 		})
 	})
 })
