76ccf64b9051a5dfc9d705d8b0a12ea902f7546b
RLRabinowitz <rlrabinowitz2@gmail.com>
2022-03-03 17:23:44 +0200

    Change environment variable test to be on deployment scope

--- tests/integration/012_environment/main.tf
+++ tests/integration/012_environment/main.tf
@@ -26,16 +26,11 @@ data "env0_environment" "test" {
   id = env0_environment.example.id
 }
 
-resource "time_sleep" "wait_30_seconds" {
-  depends_on      = [data.env0_environment.test]
-  create_duration = "30s"
-}
-
 
 data "env0_configuration_variable" "env_config_variable" {
-  depends_on     = [time_sleep.wait_30_seconds] // configuration variable scope update to "ENVIRONMENT" only after the deployment approved
-  environment_id = data.env0_environment.test.id
-  name           = "environment configuration variable"
+  depends_on        = [data.env0_environment.test]
+  deployment_log_id = data.env0_environment.test.deployment_id
+  name              = "deployment configuration variable"
 }
 
 output "name" {
