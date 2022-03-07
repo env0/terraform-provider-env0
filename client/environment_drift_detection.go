dc2d1d28478e2913865da7ef818f99fdba7df943
Elbaz <eran.elbaz@env0.com>
2022-03-07 12:33:12 +0200

    add client

---
+++ client/environment_drift_detection.go
@@ -0,0 +1,31 @@
+package client
+
+func (self *ApiClient) EnvironmentDriftDetection(environmentId string) (EnvironmentSchedulingExpression, error) {
+	var result EnvironmentSchedulingExpression
+
+	err := self.http.Get("scheduling/drift-detection/environments/"+environmentId, nil, &result)
+	if err != nil {
+		return result, err
+	}
+	return result, nil
+}
+
+func (self *ApiClient) EnvironmentUpdateDriftDetection(environmentId string, payload EnvironmentSchedulingExpression) (EnvironmentSchedulingExpression, error) {
+	var result EnvironmentSchedulingExpression
+
+	err := self.http.Put("scheduling/drift-detection/environments/"+environmentId, payload, &result)
+	if err != nil {
+		return EnvironmentSchedulingExpression{}, err
+	}
+
+	return result, nil
+}
+
+func (self *ApiClient) EnvironmentStopDriftDetection(environmentId string) error {
+	err := self.http.Put("scheduling/drift-detection/environments/"+environmentId, EnvironmentSchedulingExpression{Enabled: false, Cron: ""}, &EnvironmentScheduling{})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
