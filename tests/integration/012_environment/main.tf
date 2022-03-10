2af7ee9a7928d673c9c4b7743fd69456688c3974
Sagi Laufer <sagi.laufer@env0.com>
2022-03-10 19:51:07 +0200

    added integration test for creating a tg-template-based environment

--- tests/integration/012_environment/main.tf
+++ tests/integration/012_environment/main.tf
@@ -10,6 +10,15 @@ resource "env0_template" "template" {
   terraform_version = "0.15.1"
 }
 
+resource "env0_template" "template_tg" {
+  name               = "Template for environment resource - tg"
+  type               = "terragrunt"
+  repository         = "https://github.com/env0/templates"
+  path               = "terragrunt/misc/null-resource"
+  terraform_version  = "0.15.1"
+  terragrunt_version = "0.35.0"
+}
+
 resource "env0_environment" "example" {
   force_destroy = true
   name          = "environment"
@@ -23,12 +32,33 @@ resource "env0_environment" "example" {
   revision                   = "master"
 }
 
+resource "env0_environment" "example_tg" {
+  force_destroy = true
+  name          = "environment-tg"
+  project_id    = env0_project.test_project.id
+  template_id   = env0_template.template_tg.id
+  configuration {
+    name  = "environment configuration variable"
+    value = "value"
+  }
+  approve_plan_automatically = true
+  revision                   = "master"
+}
+
 data "env0_environment" "test" {
   id = env0_environment.example.id
 }
 
+data "env0_environment" "test_tg" {
+  id = env0_environment.example_tg.id
+}
+
 output "revision" {
   value = data.env0_environment.test.revision
+}
+
+output "active_tg" {
+  value = data.env0_environment.test_tg.status
 }
 
 
