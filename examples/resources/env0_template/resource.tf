90108ea86deeb0acd444a938ce4c3843bae0764f
Sagi Laufer <sagi.laufer@env0.com>
2022-03-16 12:11:23 +0200

    added example for terragrunt template in the examples

--- examples/resources/env0_template/resource.tf
+++ examples/resources/env0_template/resource.tf
@@ -14,6 +14,16 @@ resource "env0_template" "example" {
   ssh_keys    = [data.env0_ssh_key.my_key]
 }
 
+resource "env0_template" "example_terragrunt" {
+  name               = "example - Terragrunt"
+  description        = "Example template with Terragrunt version"
+  repository         = "https://github.com/env0/templates"
+  path               = "terragrunt/misc/null-resource"
+  ssh_keys           = [data.env0_ssh_key.my_key]
+  type               = "terragrunt"
+  terragrunt_version = "0.35.0"
+}
+
 resource "env0_template_project_assignment" "assignment" {
   template_id = env0_template.example.id
   project_id  = data.env0_project.default_project.id
