9c008ee4e7cbb5228802f8de823e411d89832806
Tomer Heber <theber@zscaler.com>
2022-03-30 15:29:12 -0500

    Feat: add git token resource (#309)
    * Feat: add git token resource
    
    * modified integration test
    
    * minor changes based CR comments

---
+++ tests/integration/019_git_token/main.tf
@@ -0,0 +1,13 @@
+resource "env0_git_token" "test_git_token" {
+  name  = "name"
+  value = "value"
+}
+
+data "env0_git_token" "test_git_token1" {
+  name       = env0_git_token.test_git_token.name
+  depends_on = [env0_git_token.test_git_token]
+}
+
+data "env0_git_token" "test_git_token2" {
+  id = env0_git_token.test_git_token.id
+}
