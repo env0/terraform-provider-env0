6c0e8ebc51b1e2d1c1cf1f1d3a9c6ee6e41e7704
Tomer Heber <theber@zscaler.com>
2022-04-07 12:14:13 -0500

    Feat: add support for agent settings (#323)

---
+++ client/agent_project_assignment.go
@@ -0,0 +1,43 @@
+package client
+
+// Key is project id.
+// Value is agent id.
+type AssignProjectsAgentsAssignmentsPayload map[string]interface{}
+
+type ProjectsAgentsAssignments struct {
+	DefaultAgent   string                 `json:"defaultAgent"`
+	ProjectsAgents map[string]interface{} `json:"ProjectsAgents"`
+}
+
+func (client *ApiClient) AssignAgentsToProjects(payload AssignProjectsAgentsAssignmentsPayload) (*ProjectsAgentsAssignments, error) {
+	organizationId, err := client.organizationId()
+	if err != nil {
+		return nil, err
+	}
+
+	var result ProjectsAgentsAssignments
+	if err := client.http.Post("/agents/projects-assignments?organizationId="+organizationId, payload, &result); err != nil {
+		return nil, err
+	}
+
+	return &result, nil
+}
+
+func (client *ApiClient) ProjectsAgentsAssignments() (*ProjectsAgentsAssignments, error) {
+	organizationId, err := client.organizationId()
+	if err != nil {
+		return nil, err
+	}
+
+	var result ProjectsAgentsAssignments
+	err = client.http.Get("/agents/projects-assignments", map[string]string{"organizationId": organizationId}, &result)
+	if err != nil {
+		return nil, err
+	}
+
+	return &result, nil
+}
+
+/*
+
+ */
