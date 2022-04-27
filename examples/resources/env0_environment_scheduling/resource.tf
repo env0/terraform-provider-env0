5ccfe13a9a9388af38e3a77ff7d0402d973b9e32
elbaz <eran.elbaz@env0.com>
2022-04-27 12:58:37 +0300

    Fix: typo in our example (#344)

--- examples/resources/env0_environment_scheduling/resource.tf
+++ examples/resources/env0_environment_scheduling/resource.tf
@@ -2,7 +2,7 @@ data "env0_environment" "example" {
   name = "Environment Name"
 }
 
-resource "env0_environment_schedling" "example" {
+resource "env0_environment_scheduling" "example" {
   environment_id = data.env0_environment.example.id
   deploy_cron    = "5 * * * *"
   destroy_cron   = "10 * * * *"
