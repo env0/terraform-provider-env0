ee54312c4a3a1080d358ed71b0717776570504a8
Yaron Yarimi <yaronya@users.noreply.github.com>
2022-05-01 16:40:32 +0300

    Chore: main page documentation (#355)

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,4 +1,16 @@
+# Terraform 0.13+ uses the Terraform Registry:
+
+terraform {
+  required_providers {
+    env0 = {
+      source = "env0/env0"
+    }
+  }
+}
+
+# Configure the env0 provider
+
 provider "env0" {
-  api_key    = var.env0_api_key    # or use ENV0_API_KEY
-  api_secret = var.env0_api_secret # or use ENV0_API_SECRET
+  api_key    = var.env0_api_key
+  api_secret = var.env0_api_secret
 }
