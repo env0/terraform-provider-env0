9efdc587db745a791cd9bda33a4362c1ad595672
Tomer Heber <theber@zscaler.com>
2022-05-09 09:33:15 -0500

    Fix: env0_cost_credentials_project_assignment not working

---
+++ tests/integration/023_cost_credentials_project_assignment/main.tf
@@ -0,0 +1,23 @@
+provider "random" {}
+
+resource "random_string" "random" {
+  length    = 8
+  special   = false
+  min_lower = 8
+}
+
+resource "env0_project" "project" {
+  name        = "Test-Project-${random_string.random.result}"
+  description = "description"
+}
+
+resource "env0_aws_cost_credentials" "cost" {
+  name        = "cost-${random_string.random.result}"
+  arn         = "arn"
+  external_id = "externalid"
+}
+
+resource "env0_cost_credentials_project_assignment" "cost_project_assignment" {
+  credential_id = env0_aws_cost_credentials.cost.id
+  project_id    = env0_project.project.id
+}
