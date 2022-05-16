0cba7ad5e78fe7f1b4f3d71046e3c64a87d69945
GiliFaroEnv0 <gili.faro@env0.com>
2022-05-16 15:39:31 +0300

    fix: add random string to notification names to prevent name collision

--- tests/integration/017_notification/main.tf
+++ tests/integration/017_notification/main.tf
@@ -1,11 +1,15 @@
+resource "random_string" "random_name" {
+  length = 20
+}
+
 resource "env0_notification" "test_notification" {
-  name  = var.second_run ? "Test-some-other-name" : "Test-Notification"
+  name  = var.second_run ? "Test-some-other-name ${random_string.random_name.result}" : "Test-Notification ${random_string.random_name.result}"
   type  = var.second_run ? "Slack" : "Teams"
   value = var.second_run ? "https://someotherurl.com" : "https://someurl.com"
 }
 
 resource "env0_project" "test_project" {
-  name        = "Test-Project-For-Notification"
+  name        = "Test-Project-For-Notification ${random_string.random_name.result}"
   description = "Test Description"
 }
 
