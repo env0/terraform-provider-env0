8b67bfacf9e22de3ebde79a733aa720f5732e781
Tomer Heber <theber@zscaler.com>
2022-05-27 08:52:52 -0500

    Feat: specify HCL in the env0_enviroment resource configuration

--- examples/resources/env0_environment/resource.tf
+++ examples/resources/env0_environment/resource.tf
@@ -12,3 +12,22 @@ resource "env0_environment" "example" {
   template_id = data.env0_template.example.id
 }
 
+resource "env0_environment" "example_with_hcl_configuration" {
+  name                             = "environment with hcl"
+  project_id                       = data.env0_project.default_project.id
+  template_id                      = data.env0_template.example.id
+
+  configuration {
+    name          = "TEST1234"
+    type          = "terraform"
+    value         = <<EOF
+      {
+        a = "world11111"
+        b = {
+          c = "d"
+        }
+      }
+    EOF
+    schema_format = "HCL"
+  }
+}
