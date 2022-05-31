c40a0467dbc1fd49127c521149a74e5fb7de3701
Tomer Heber <theber@zscaler.com>
2022-05-31 07:51:57 -0500

    Feat: add data source for getting a list of notification targets (#403)

---
+++ env0/data_notifications.go
@@ -0,0 +1,46 @@
+package env0
+
+import (
+	"context"
+
+	"github.com/env0/terraform-provider-env0/client"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func dataNotifications() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: dataNotificationsRead,
+
+		Schema: map[string]*schema.Schema{
+			"names": {
+				Type:        schema.TypeList,
+				Description: "list of all notifications (by name)",
+				Computed:    true,
+				Elem: &schema.Schema{
+					Type:        schema.TypeString,
+					Description: "the notification name",
+				},
+			},
+		},
+	}
+}
+
+func dataNotificationsRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	apiClient := meta.(client.ApiClientInterface)
+	notifications, err := apiClient.Notifications()
+	if err != nil {
+		return diag.Errorf("could not get notifications: %v", err)
+	}
+
+	names := []string{}
+
+	for _, notification := range notifications {
+		names = append(names, notification.Name)
+	}
+
+	d.Set("names", names)
+	d.SetId("all_notification_names")
+
+	return nil
+}
---
+++ env0/data_notifications_test.go
@@ -0,0 +1,73 @@
+package env0
+
+import (
+	"errors"
+	"regexp"
+	"testing"
+
+	"github.com/env0/terraform-provider-env0/client"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestNotificationsDataSource(t *testing.T) {
+	notification1 := client.Notification{
+		Id:    "id0",
+		Name:  "name0",
+		Type:  client.NotificationTypeSlack,
+		Value: "http://my-notification-0.com",
+	}
+
+	notification2 := client.Notification{
+		Id:    "id1",
+		Name:  "name10",
+		Type:  client.NotificationTypeTeams,
+		Value: "http://my-notification-0.com",
+	}
+
+	resourceType := "env0_notifications"
+	resourceName := "test_notifications"
+	accessor := dataSourceAccessor(resourceType, resourceName)
+
+	getTestCase := func() resource.TestCase {
+		return resource.TestCase{
+			Steps: []resource.TestStep{
+				{
+					Config: dataSourceConfigCreate(resourceType, resourceName, map[string]interface{}{}),
+					Check: resource.ComposeAggregateTestCheckFunc(
+						resource.TestCheckResourceAttr(accessor, "names.0", notification1.Name),
+						resource.TestCheckResourceAttr(accessor, "names.1", notification2.Name),
+					),
+				},
+			},
+		}
+	}
+
+	mockNotifications := func(returnValue []client.Notification) func(mockFunc *client.MockApiClientInterface) {
+		return func(mock *client.MockApiClientInterface) {
+			mock.EXPECT().Notifications().AnyTimes().Return(returnValue, nil)
+		}
+	}
+
+	t.Run("Success", func(t *testing.T) {
+		runUnitTest(t,
+			getTestCase(),
+			mockNotifications([]client.Notification{notification1, notification2}),
+		)
+	})
+
+	t.Run("API Call Error", func(t *testing.T) {
+		runUnitTest(t,
+			resource.TestCase{
+				Steps: []resource.TestStep{
+					{
+						Config:      dataSourceConfigCreate(resourceType, resourceName, map[string]interface{}{}),
+						ExpectError: regexp.MustCompile("error"),
+					},
+				},
+			},
+			func(mock *client.MockApiClientInterface) {
+				mock.EXPECT().Notifications().AnyTimes().Return(nil, errors.New("error"))
+			},
+		)
+	})
+}
