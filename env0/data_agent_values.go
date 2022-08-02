d2cc8907e7cc9ace9fa445401c658b6a0f365cbd
Tomer Heber <theber@zscaler.com>
2022-08-02 02:41:52 -0500

    Feat: add Data source for self-hosted-agent helm values (#458)

---
+++ env0/data_agent_values.go
@@ -0,0 +1,44 @@
+package env0
+
+import (
+	"context"
+
+	"github.com/env0/terraform-provider-env0/client"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func dataAgentValues() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: dataAgentValuesRead,
+
+		Schema: map[string]*schema.Schema{
+			"agent_key": {
+				Type:        schema.TypeString,
+				Description: "the agent key",
+				Required:    true,
+			},
+			"values": {
+				Type:        schema.TypeString,
+				Description: "Self hosted agent helm values. The values can be passed to a helm release resource (https://registry.terraform.io/providers/hashicorp/helm/latest/docs/resources/release)",
+				Computed:    true,
+			},
+		},
+	}
+}
+
+func dataAgentValuesRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	apiClient := meta.(client.ApiClientInterface)
+
+	agentKey := d.Get("agent_key").(string)
+
+	values, err := apiClient.AgentValues(agentKey)
+	if err != nil {
+		return diag.Errorf("could not get agent values: %v", err)
+	}
+
+	d.Set("values", values)
+	d.SetId(agentKey)
+
+	return nil
+}
