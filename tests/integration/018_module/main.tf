db1d55e1a6a9ff75c669f5f79fc441b8354ab928
Yaron Yarimi <yaron.yarimi@env0.com>
2022-08-18 10:51:03 +0300

    make sure tf module name is unique in integration tests

--- tests/integration/018_module/main.tf
+++ tests/integration/018_module/main.tf
@@ -1,5 +1,13 @@
+provider "random" {}
+
+resource "random_string" "random" {
+  length    = 8
+  special   = false
+  min_lower = 8
+}
+
 resource "env0_module" "test_module" {
-  module_name            = "test-module234"
+  module_name            = "test-module-${random_string.random.result}"
   module_provider        = "testprovider1"
   token_id               = var.second_run ? null : "37689e5a-5298-4555-b71e-92b80f736222"
   token_name             = var.second_run ? null : "johns_token"
