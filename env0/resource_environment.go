3c3799e23c67dc5e30326551da859d66626695cd
eladmoshe <93523957+eladmosh@users.noreply.github.com>
2022-09-29 17:01:42 +0300

    Fix: tf apply fails when switching from v1.2.4 to v1.2.5 (#509)
    * extracting bluprintId from lastDeployment
    
    * fix tests
    
    * formatting
    
    * checking for environment.BlueprintId also

--- env0/resource_environment.go
+++ env0/resource_environment.go
@@ -241,10 +241,16 @@ func setEnvironmentSchema(d *schema.ResourceData, environment client.Environment
 			d.Set("template_id", environment.LatestDeploymentLog.BlueprintId)
 			d.Set("revision", environment.LatestDeploymentLog.BlueprintRevision)
 		}
-	} else if environment.BlueprintId != "" {
+	} else if environment.BlueprintId != "" || environment.LatestDeploymentLog.BlueprintId != "" {
 		settings := d.Get("without_template_settings").([]interface{})
 		elem := settings[0].(map[string]interface{})
-		elem["id"] = environment.BlueprintId
+
+		if environment.BlueprintId != "" {
+			elem["id"] = environment.BlueprintId
+		} else {
+			elem["id"] = environment.LatestDeploymentLog.BlueprintId
+		}
+
 		d.Set("without_template_settings", settings)
 	}
 
