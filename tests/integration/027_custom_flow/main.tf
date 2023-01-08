cd9a09bec02c43c302b05e62d929cf38bf3e3cf8
Tomer Heber <theber@zscaler.com>
2023-01-08 09:20:49 -0600

    fix integration test

--- tests/integration/027_custom_flow/main.tf
+++ tests/integration/027_custom_flow/main.tf
@@ -14,5 +14,5 @@ resource "env0_custom_flow" "test" {
   name                   = "Custom Flow Github Test ${random_string.random.result}"
   repository             = data.env0_template.github_template.repository
   github_installation_id = data.env0_template.github_template.github_installation_id
-  path                   = "misc/null-resource"
+  path                   = "custom-flows/opa.yaml"
 }
