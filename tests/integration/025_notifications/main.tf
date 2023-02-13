46103a8b42d900da7ca8a6b0e7bc59b0693ed2ad
Tomer Heber <theber@zscaler.com>
2023-02-13 09:52:46 -0600

    removed rarity of notification

--- tests/integration/025_notifications/main.tf
+++ tests/integration/025_notifications/main.tf
@@ -1,20 +1,19 @@
 provider "random" {}
 
 resource "random_string" "random" {
-  length    = 8
+  length    = 20
   special   = false
-  min_lower = 8
+  min_lower = 20
 }
 
-
 resource "env0_notification" "test_notification_1" {
-  name  = "notification-${random_string.random.result}-1"
+  name  = "notification123-${random_string.random.result}-1"
   type  = "Slack"
   value = "https://someurl1.com"
 }
 
 resource "env0_notification" "test_notification_2" {
-  name  = "notification-${random_string.random.result}-2"
+  name  = "notification123-${random_string.random.result}-2"
   type  = "Teams"
   value = "https://someurl2.com"
 }
