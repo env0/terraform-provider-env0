ead551f0805b41c5c4b1a5c7cef419fffc9ca01d
Tomer Heber <heber.tomer@gmail.com>
2023-05-04 07:03:33 -0500

    Feat: ability to filter for parent project name in data project resource (#639)
    * Feat: ability to filter for parent project name in data project resource
    
    * some minor refactoring
    
    * removed computed
    
    * some minor changes based on PR feedback
    
    * minor fixes
    
    * always filter based on parent name if exist

--- env0/data_project_test.go
+++ env0/data_project_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/env0/terraform-provider-env0/client"
 	"github.com/env0/terraform-provider-env0/client/http"
+	"github.com/golang/mock/gomock"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
@@ -26,6 +27,40 @@ func TestProjectDataSource(t *testing.T) {
 		Role:        project.Role,
 		Description: project.Description,
 		IsArchived:  true,
+	}
+
+	parentProject := client.Project{
+		Id:          "id_parent1",
+		Name:        "parent_project_name",
+		CreatedBy:   "env0",
+		Role:        "role0",
+		Description: "A project's description",
+	}
+
+	otherParentProject := client.Project{
+		Id:          "id_parent2",
+		Name:        "other_parent_project_name",
+		CreatedBy:   "env0",
+		Role:        "role0",
+		Description: "A project's description",
+	}
+
+	projectWithParent := client.Project{
+		Id:              "id123",
+		Name:            "same_name",
+		CreatedBy:       "env0",
+		Role:            "role0",
+		Description:     "A project's description",
+		ParentProjectId: parentProject.Id,
+	}
+
+	otherProjectWithParent := client.Project{
+		Id:              "id234",
+		Name:            "same_name",
+		CreatedBy:       "env0",
+		Role:            "role0",
+		Description:     "A project's description",
+		ParentProjectId: otherParentProject.Id,
 	}
 
 	projectDataByName := map[string]interface{}{"name": project.Name}
@@ -96,6 +131,31 @@ func TestProjectDataSource(t *testing.T) {
 		)
 	})
 
+	t.Run("By Name with Parent Name", func(t *testing.T) {
+		runUnitTest(t,
+			resource.TestCase{
+				Steps: []resource.TestStep{
+					{
+						Config: dataSourceConfigCreate(resourceType, resourceName, map[string]interface{}{"name": projectWithParent.Name, "parent_project_name": parentProject.Name}),
+						Check: resource.ComposeAggregateTestCheckFunc(
+							resource.TestCheckResourceAttr(accessor, "id", projectWithParent.Id),
+							resource.TestCheckResourceAttr(accessor, "name", projectWithParent.Name),
+							resource.TestCheckResourceAttr(accessor, "created_by", projectWithParent.CreatedBy),
+							resource.TestCheckResourceAttr(accessor, "role", projectWithParent.Role),
+							resource.TestCheckResourceAttr(accessor, "description", projectWithParent.Description),
+							resource.TestCheckResourceAttr(accessor, "parent_project_id", projectWithParent.ParentProjectId),
+						),
+					},
+				},
+			},
+			func(mock *client.MockApiClientInterface) {
+				mock.EXPECT().Projects().AnyTimes().Return([]client.Project{projectWithParent, otherProjectWithParent}, nil)
+				mock.EXPECT().Project(projectWithParent.ParentProjectId).AnyTimes().Return(parentProject, nil)
+				mock.EXPECT().Project(otherProjectWithParent.ParentProjectId).AnyTimes().Return(otherParentProject, nil)
+			},
+		)
+	})
+
 	t.Run("Throw error when no name or id is supplied", func(t *testing.T) {
 		runUnitTest(t,
 			getErrorTestCase(map[string]interface{}{}, "one of `id,name` must be specified"),
@@ -105,8 +165,22 @@ func TestProjectDataSource(t *testing.T) {
 
 	t.Run("Throw error when by name and more than one project exists", func(t *testing.T) {
 		runUnitTest(t,
-			getErrorTestCase(projectDataByName, "found multiple Projects for name"),
+			getErrorTestCase(projectDataByName, "found multiple projects for name"),
 			mockListProjectsCall([]client.Project{project, project}),
+		)
+	})
+
+	t.Run("Throw error when by name and more than one project and parent project name exist", func(t *testing.T) {
+		runUnitTest(t,
+			getErrorTestCase(map[string]interface{}{"name": projectWithParent.Name, "parent_project_name": parentProject.Name}, "found multiple projects for name"),
+			func(mock *client.MockApiClientInterface) {
+				gomock.InOrder(
+					mock.EXPECT().Projects().Times(1).Return([]client.Project{projectWithParent, otherProjectWithParent, projectWithParent}, nil),
+					mock.EXPECT().Project(projectWithParent.ParentProjectId).Times(1).Return(parentProject, nil),
+					mock.EXPECT().Project(otherProjectWithParent.ParentProjectId).Times(1).Return(otherParentProject, nil),
+					mock.EXPECT().Project(projectWithParent.ParentProjectId).Times(1).Return(parentProject, nil),
+				)
+			},
 		)
 	})
 
@@ -122,6 +196,18 @@ func TestProjectDataSource(t *testing.T) {
 		runUnitTest(t,
 			getErrorTestCase(projectWithOtherName, "could not find a project with name"),
 			mockListProjectsCall([]client.Project{project, project}),
+		)
+	})
+
+	t.Run("Throw error when by name and no projects found with that name and parent name", func(t *testing.T) {
+		runUnitTest(t,
+			getErrorTestCase(map[string]interface{}{"name": projectWithParent.Name, "parent_project_name": "noooo"}, "could not find a project with name"),
+			func(mock *client.MockApiClientInterface) {
+				gomock.InOrder(
+					mock.EXPECT().Projects().Times(1).Return([]client.Project{projectWithParent}, nil),
+					mock.EXPECT().Project(projectWithParent.ParentProjectId).Times(1).Return(parentProject, nil),
+				)
+			},
 		)
 	})
 
