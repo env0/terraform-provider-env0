b33c6b13999db4ef182b0826ed93fe407ccf440a
Tomer Heber <heber.tomer@gmail.com>
2023-06-30 15:51:38 -0500

    Feat: run harness tests in parallel (#669)

--- tests/harness.go
+++ tests/harness.go
@@ -9,6 +9,7 @@ import (
 	"path"
 	"runtime"
 	"strings"
+	"sync"
 )
 
 const TESTS_FOLDER = "tests/integration"
@@ -24,16 +25,27 @@ func main() {
 	log.Println(len(testNames), " tests to run")
 	buildFakeTerraformRegistry()
 	destroyMode := os.Getenv("DESTROY_MODE")
+
+	var wg sync.WaitGroup
+
 	for _, testName := range testNames {
-		if destroyMode == "DESTROY_ONLY" {
-			terraformDestory(testName)
-		} else {
-			success, err := runTest(testName, destroyMode != "NO_DESTROY")
-			if !success {
-				log.Fatalln("Halting due to test failure:", err)
+		wg.Add(1)
+
+		go func(testName string) {
+			if destroyMode == "DESTROY_ONLY" {
+				terraformDestory(testName)
+			} else {
+				success, err := runTest(testName, destroyMode != "NO_DESTROY")
+				if !success {
+					log.Fatalln("Halting due to test failure:", err)
+				}
 			}
-		}
+
+			wg.Done()
+		}(testName)
 	}
+
+	wg.Wait()
 }
 func compileProvider() error {
 	cmd := exec.Command("go", "build")
