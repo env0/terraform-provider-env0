2c1c6d7d0f6736d4da36e714c112531f8d9d6b7f
GiliFaroEnv0 <92937594+GiliFaroEnv0@users.noreply.github.com>
2023-10-04 15:39:12 +0300

    Fix: make cloud_credentials data test isolated (#721)
    * Fix: make cloud_credentials data test isolated

--- tests/integration/024_cloud_credentials/main.tf
+++ tests/integration/024_cloud_credentials/main.tf
@@ -4,26 +4,35 @@ resource "random_string" "random" {
   min_lower = 8
 }
 
-resource "env0_aws_credentials" "cred1" {
+resource "env0_aws_credentials" "aws_cred1" {
   name = "Test Role arn1 ${random_string.random.result}"
   arn  = "Role ARN1"
 }
 
-resource "env0_gcp_credentials" "cred2" {
+resource "env0_aws_credentials" "aws_cred2" {
+  name = "Test Role arn2 ${random_string.random.result}"
+  arn  = "Role ARN2"
+}
+
+resource "env0_gcp_credentials" "gcp_cred" {
   name                = "name ${random_string.random.result}"
   service_account_key = "your_account_key"
   project_id          = "your_project_id"
 }
 
-data "env0_cloud_credentials" "aws_credentials" {
+data "env0_cloud_credentials" "all_aws_credentials" {
+  depends_on = [env0_aws_credentials.aws_cred1, env0_aws_credentials.aws_cred2, env0_gcp_credentials.gcp_cred]
   credential_type = "AWS_ASSUMED_ROLE_FOR_DEPLOYMENT"
 }
 
-data "env0_aws_credentials" "aws_credentials" {
-  for_each = toset(data.env0_cloud_credentials.aws_credentials.names)
-  name     = each.value
+data "env0_aws_credentials" "aws_credentials1" {
+  name     = data.env0_cloud_credentials.all_aws_credentials.names[index(data.env0_cloud_credentials.all_aws_credentials.names, env0_aws_credentials.aws_cred1.name)]
+}
+
+data "env0_aws_credentials" "aws_credentials2" {
+  name     = data.env0_cloud_credentials.all_aws_credentials.names[index(data.env0_cloud_credentials.all_aws_credentials.names, env0_aws_credentials.aws_cred2.name)]
 }
 
 output "credentials_name" {
-  value = var.second_run ? replace(data.env0_aws_credentials.aws_credentials["Test Role arn1 ${random_string.random.result}"].name, random_string.random.result, "") : ""
+  value = var.second_run ? replace(data.env0_aws_credentials.aws_credentials1.name, random_string.random.result, "") : ""
 }
