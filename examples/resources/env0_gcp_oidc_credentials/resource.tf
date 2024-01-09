e7b3412bfb48ef349f65ca699a97f3a9fabe8844
Tomer Heber <theber@zscaler.com>
2024-01-09 17:43:57 -0600

    Fixed example

--- examples/resources/env0_gcp_oidc_credentials/resource.tf
+++ examples/resources/env0_gcp_oidc_credentials/resource.tf
@@ -1,9 +1,3 @@
-resource "env0_aws_oidc_credentials" "credentials" {
-  name     = "example"
-  role_arn = "arn::role::34"
-  duration = 3600
-}
-
 resource "env0_gcp_oidc_credentials" "credentials" {
   name = "example"
   credential_configuration_file_content = jsonencode({
