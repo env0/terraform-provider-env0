a76836d24ba98cbdae8f678500f1a2edc1366cf3
Tomer Heber <heber.tomer@gmail.com>
2024-05-12 11:30:48 -0500

    Feat: add a retry mechanism for getSshKeyByName (#849)

--- env0/data_sshkey_test.go
+++ env0/data_sshkey_test.go
@@ -2,11 +2,13 @@ package env0
 
 import (
 	"encoding/json"
+	"errors"
 	"regexp"
 	"testing"
 
 	"github.com/env0/terraform-provider-env0/client"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"go.uber.org/mock/gomock"
 )
 
 func TestUnitSshKeyDataSourceById(t *testing.T) {
@@ -68,5 +70,45 @@ func testUnitSshKeyDataSource(t *testing.T, byKey string) {
 	runUnitTest(t, testCase, func(mock *client.MockApiClientInterface) {
 		// TODO: AnyTimes because we find that READ runs for 5 times. need investigation.
 		mock.EXPECT().SshKeys().AnyTimes().Return([]client.SshKey{sshKey}, nil)
+	})
+}
+
+func TestUnitSshKeyDataSourceRetryByName(t *testing.T) {
+	resourceType := "env0_ssh_key"
+	resourceName := "test"
+	accessor := dataSourceAccessor(resourceType, resourceName)
+
+	sshKey := client.SshKey{
+		Id:    "id0",
+		Name:  "name0",
+		Value: "Key🔑",
+	}
+
+	otherSshKey := client.SshKey{
+		Id:    "id1",
+		Name:  "name1",
+		Value: "value1",
+	}
+
+	testCase := resource.TestCase{
+		Steps: []resource.TestStep{
+			{
+				Config: dataSourceConfigCreate(resourceType, resourceName, map[string]interface{}{
+					"name": sshKey.Name,
+				}),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(accessor, "id", sshKey.Id),
+					resource.TestCheckResourceAttr(accessor, "name", sshKey.Name),
+				),
+			},
+		},
+	}
+
+	runUnitTest(t, testCase, func(mock *client.MockApiClientInterface) {
+		gomock.InOrder(
+			mock.EXPECT().SshKeys().Times(1).Return(nil, errors.New("error")),
+			mock.EXPECT().SshKeys().Times(1).Return([]client.SshKey{otherSshKey}, nil),
+			mock.EXPECT().SshKeys().AnyTimes().Return([]client.SshKey{sshKey, otherSshKey}, nil),
+		)
 	})
 }
