550fd66d821d99d34b74b3e39af6cd151288cd9d
Tomer Heber <theber@zscaler.com>
2024-05-27 14:53:14 -0500

    fix example

--- examples/resources/env0_aws_eks_credentials/import.sh
+++ examples/resources/env0_aws_eks_credentials/import.sh
@@ -1,2 +1,2 @@
-terraform import env0_aws_oidc_credentials.by_id d31a6b30-5f69-4d24-937c-22322754934e
-terraform import env0_aws_oidc_credentials.by_name "credentials name"
+terraform import env0_aws_eks_credentials.by_id d31a6b30-5f69-4d24-937c-22322754934e
+terraform import env0_aws_eks_credentials.by_name "credentials name"
