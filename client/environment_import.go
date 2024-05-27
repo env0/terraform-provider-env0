2a92a9bb21b2bb4067c98c45029bdb4114b64f8a
Alon Noga <alon@Alons-Laptop.local>
2024-05-27 18:06:45 +0300

    remove varaible from env import client

--- client/environment_import.go
+++ client/environment_import.go
@@ -1,21 +1,13 @@
 package client
 
 type EnvironmentImport struct {
-	Id             string     `json:"id"`
-	Name           string     `json:"name"`
-	OrganizationId string     `json:"organizationId"`
-	Workspace      string     `json:"workspace"`
-	Variables      []Variable `json:"variables"`
-	GitConfig      GitConfig  `json:"gitConfig"`
-	IacType        string     `json:"iacType"` // opentofu or terraform
-	IacVersion     string     `json:"iacVersion"`
-}
-
-type Variable struct {
-	Name        string `json:"name"`
-	Value       string `json:"value,omitempty"`
-	IsSensitive bool   `json:"isSensitive"`
-	Type        string `json:"type"` // string or JSON
+	Id             string    `json:"id"`
+	Name           string    `json:"name"`
+	OrganizationId string    `json:"organizationId"`
+	Workspace      string    `json:"workspace"`
+	GitConfig      GitConfig `json:"gitConfig"`
+	IacType        string    `json:"iacType"` // opentofu or terraform
+	IacVersion     string    `json:"iacVersion"`
 }
 
 type GitConfig struct {
@@ -26,20 +18,18 @@ type GitConfig struct {
 }
 
 type EnvironmentImportCreatePayload struct {
-	Name       string     `json:"name,omitempty"`
-	Workspace  string     `json:"workspace,omitempty"`
-	Variables  []Variable `json:"variables,omitempty"`
-	GitConfig  GitConfig  `json:"gitConfig,omitempty"`
-	IacType    string     `json:"iacType,omitempty"`
-	IacVersion string     `json:"iacVersion,omitempty"`
+	Name       string    `json:"name,omitempty"`
+	Workspace  string    `json:"workspace,omitempty"`
+	GitConfig  GitConfig `json:"gitConfig,omitempty"`
+	IacType    string    `json:"iacType,omitempty"`
+	IacVersion string    `json:"iacVersion,omitempty"`
 }
 
 type EnvironmentImportUpdatePayload struct {
-	Name       string     `json:"name,omitempty"`
-	Variables  []Variable `json:"variables,omitempty"`
-	GitConfig  GitConfig  `json:"gitConfig,omitempty"`
-	IacType    string     `json:"iacType,omitempty"`
-	IacVersion string     `json:"iacVersion,omitempty"`
+	Name       string    `json:"name,omitempty"`
+	GitConfig  GitConfig `json:"gitConfig,omitempty"`
+	IacType    string    `json:"iacType,omitempty"`
+	IacVersion string    `json:"iacVersion,omitempty"`
 }
 
 func (client *ApiClient) EnvironmentImportCreate(payload *EnvironmentImportCreatePayload) (*EnvironmentImport, error) {
