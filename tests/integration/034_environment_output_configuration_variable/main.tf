fb882bb1a1e320539b10a4fe67d4141e97d69395
Tomer Heber <theber@zscaler.com>
2024-07-10 12:38:11 -0500

    changed id

--- tests/integration/034_environment_output_configuration_variable/main.tf
+++ tests/integration/034_environment_output_configuration_variable/main.tf
@@ -16,6 +16,6 @@ resource "env0_environment_output_configuration_variable" "to_project" {
   scope_id              = env0_project.project.id
   type                  = "terraform"
   description           = "description"
-  output_environment_id = "d73c4f0c-5569-44c6-936f-25a042b1aedf"
+  output_environment_id = "aa3b02cd-bc82-4a1c-88e0-fb73f74dfdce"
   output_name           = "not_sensitive_output"
 }
