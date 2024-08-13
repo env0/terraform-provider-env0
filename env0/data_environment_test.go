49ed41e2f4e95e913215aa9c1f50f6e656ef7e8e
Yaron Yarimi <yaron.yarimi@env0.com>
2024-08-13 21:20:31 +0300

    fix test

--- env0/data_environment_test.go
+++ env0/data_environment_test.go
@@ -109,7 +109,7 @@ func TestEnvironmentDataSource(t *testing.T) {
 
 	mockListEnvironmentsCall := func(returnValue []client.Environment, tem *client.Template) func(mockFunc *client.MockApiClientInterface) {
 		return func(mock *client.MockApiClientInterface) {
-			mock.EXPECT().Environments().AnyTimes().Return(returnValue, nil)
+			mock.EXPECT().EnvironmentsByName(environment.Name).AnyTimes().Return(returnValue, nil)
 			if tem != nil {
 				mock.EXPECT().Template(environment.LatestDeploymentLog.BlueprintId).AnyTimes().Return(*tem, nil)
 			}
