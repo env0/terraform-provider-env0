297ae57e7215d6d0ba1468c1a083de145014d942
Marcelo Luiz Onhate <onhate@gmail.com>
2024-11-12 09:45:10 -0300

    chore: Integration test for the bug not possible to use OpenTofu as backend for Terragrunt via env0 Terraform provider

--- tests/integration/036_environment_without_template_settings_terragrunt_opentofu/main.tf
+++ tests/integration/036_environment_without_template_settings_terragrunt_opentofu/main.tf
@@ -12,13 +12,10 @@ resource "env0_project" "project" {
 }
 
 resource "env0_environment" "environment" {
-  name                      = "environment-without_template-${random_string.random.result}"
-  project_id                = env0_project.project.id
-  is_remote_backend         = false
-  deploy_on_push            = true
-  run_plan_on_pull_requests = true
-  vcs_pr_comments_enabled   = true
-  force_destroy             = true
+  name              = "environment-without_template-${random_string.random.result}"
+  project_id        = env0_project.project.id
+  is_remote_backend = false
+  force_destroy     = true
 
   without_template_settings {
     description           = "Core Azure"
