8a80a6aa14f77ca69acad9f946fc8e08598cd97b
GiliFaroEnv0 <92937594+GiliFaroEnv0@users.noreply.github.com>
2024-12-03 21:57:20 +0200

    Fix: increase sleep time to avoid flaky test (#985)

--- tests/integration/003_configuration_variable/main.tf
+++ tests/integration/003_configuration_variable/main.tf
@@ -31,8 +31,8 @@ resource "env0_configuration_variable" "regex_var" {
   regex      = "^test-\\d+$"
 }
 
-resource "time_sleep" "wait_5_seconds" {
-  create_duration = "5s"
+resource "time_sleep" "wait_10_seconds" {
+  create_duration = "10s"
 
   depends_on = [env0_configuration_variable.region_in_project_resource, env0_configuration_variable.tested1, env0_configuration_variable.tested3, env0_configuration_variable.regex_var]
 }
@@ -41,7 +41,7 @@ data "env0_configuration_variable" "region_in_project" {
   name       = "AWS_DEFAULT_REGION"
   project_id = env0_project.test_project.id
 
-  depends_on = [time_sleep.wait_5_seconds]
+  depends_on = [time_sleep.wait_10_seconds]
 }
 output "region_in_project_value" {
   value     = data.env0_configuration_variable.region_in_project.value
@@ -55,7 +55,7 @@ output "region_in_project_id" {
 data "env0_configuration_variable" "tested1" {
   name = "tested1-${random_string.random.result}"
 
-  depends_on = [time_sleep.wait_5_seconds, env0_configuration_variable.tested1]
+  depends_on = [time_sleep.wait_10_seconds, env0_configuration_variable.tested1]
 }
 
 output "tested1_value" {
@@ -66,14 +66,14 @@ output "tested1_value" {
 data "env0_configuration_variable" "tested2" {
   id = env0_configuration_variable.tested1.id
 
-  depends_on = [time_sleep.wait_5_seconds]
+  depends_on = [time_sleep.wait_10_seconds]
 }
 
 
 data "env0_configuration_variable" "tested3" {
   name = "tested3-${random_string.random.result}"
 
-  depends_on = [time_sleep.wait_5_seconds]
+  depends_on = [time_sleep.wait_10_seconds]
 }
 
 output "tested3_enum_1" {
@@ -89,7 +89,7 @@ data "env0_configuration_variable" "regex_var" {
   project_id = env0_project.test_project.id
   name       = "regex_var"
 
-  depends_on = [time_sleep.wait_5_seconds]
+  depends_on = [time_sleep.wait_10_seconds]
 }
 
 output "regex" {
