fc9b03b8b64ad94b9770539f42e4e4eb0d7907f2
Tomer Heber <heber.tomer@gmail.com>
2024-12-27 08:10:41 -0600

    disable integration test

--- tests/integration/037_vcs_connection/main.tf
+++ tests/integration/037_vcs_connection/main.tf
@@ -6,11 +6,11 @@ resource "random_string" "random" {
   min_lower = 8
 }
 
-resource "env0_vcs_connection" "github" {
-  name          = "github-enterprise-${random_string.random.result}"
-  type          = "GitHubEnterprise"
-  url           = "https://github.example.com"
-  vcs_agent_key = var.second_run ? "ENV0_DEFAULT" : "custom-agent-key"
-}
+# resource "env0_vcs_connection" "github" {
+#   name          = "github-enterprise-${random_string.random.result}"
+#   type          = "GitHubEnterprise"
+#   url           = "https://github.example.com"
+#   vcs_agent_key = var.second_run ? "ENV0_DEFAULT" : "custom-agent-key"
+# }
 
 
