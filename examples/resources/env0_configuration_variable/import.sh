0a6033ef7385545ce276115b3fbd8e6812713733
Loraine Kanervisto <loraine.kanervisto@afreshtechnologies.com>
2025-05-08 17:21:48 -0700

    Update import example

--- examples/resources/env0_configuration_variable/import.sh
+++ examples/resources/env0_configuration_variable/import.sh
@@ -1,1 +1,4 @@
-terraform import env0_configuration_variable.my_config '{  "Scope": "PROJECT",  "ScopeId": "project id",  "name": "configuration variable name"}'
+# For project variables: include a resource block that passes the project_id argument
+terraform import env0_configuration_variable.my_project_config '{ "Scope": "PROJECT", "ScopeId": "project id", "name": "configuration variable name"}'
+# For global variables: do not pass ScopeId in the import commant
+terraform import env0_configuration_variable.my_global_config '{ "Scope": "GLOBAL", "name": "configuration variable name"}'
