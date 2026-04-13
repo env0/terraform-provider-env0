2bad72517f0a02ec7761af79663cb2a14449aed3
Chaim Platonov <chaim.platonov@env0.com>
2026-04-13 16:12:15 +0300

    deprecation

--- examples/resources/env0_agent_pool/resource_logs.tf
+++ examples/resources/env0_agent_pool/resource_logs.tf
@@ -16,6 +16,6 @@ resource "env0_agent_pool" "agent_with_logs_config" {
 module "agent_logs" {
   source = "github.com/env0/k8s-modules?ref=v1.1.0//log-storage/aws/dynamodb"
 
-  agent_key   = env0_agent_pool.agent_with_logs_config.agent_key
+  agent_key   = env0_agent_pool.agent_with_logs_config.id
   external_id = data.env0_organization.this.id
 }
